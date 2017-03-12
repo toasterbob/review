a0675e10d933ddc37cb7a6b3ff766f079ddf9950
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-11 19:23:38 -0800

    calculator

--- react/day1/calculator/word.jsx
+++ react/day1/calculator/word.jsx
@@ -6,19 +6,18 @@ class WordInput extends React.Component {
     this.state = {
       word: ''
     };
-    this.linkState = this.linkState.bind(this);
   }
 
-  linkState(e) {
+  linkState(key) {
     // here we use '[key]' to set the key to be the value of the 'key' variable,
     // as opposed to a string of 'key'
-    this.setState({ word: event.currentTarget.value });
+    return (event => this.setState({[key]: event.currentTarget.value}));
   }
 
   render () {
     return (
       <div>
-        <input onChange={this.linkState} value={this.state.word}/>
+        <input onChange={this.linkState('word')} value={this.state.word}/>
         <span>The word is: {this.state.word}</span>
       </div>
     );
