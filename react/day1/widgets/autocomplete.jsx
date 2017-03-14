06c6d867d68a7e13d05cf68c8aef18845c598bde
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-13 20:37:23 -0700

    autocomplete

--- react/day1/widgets/autocomplete.jsx
+++ react/day1/widgets/autocomplete.jsx
@@ -3,18 +3,30 @@ import React from 'react';
 class Autocomplete extends React.Component {
   constructor(props){
     super(props);
+    this.state = { value: ""};
 
+    this.handleChange = this.handleChange.bind(this);
+    this.selectName = this.selectName.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({ value: event.target.value});
+  }
+
+  selectName(event){
+    this.setState({ value: event.currentTarget.innerText});
+    console.log(this.state.value);
   }
 
   render() {
     let names = this.props.names.map((name, idx) => {
       return(
-        <li key={idx}>{name}</li>
+        <li key={idx} onClick={this.selectName}>{name}</li>
       );
     });
     return (
       <div className="autocomplete">
-        <input placeholder="search..."></input>
+        <input value = {this.state.value} onChange = {this.handleChange} placeholder="search..."></input>
         <ul>
           {names}
         </ul>
