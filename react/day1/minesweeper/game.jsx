834cc984e853259dcf23acd375a2991ba4b77576
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-14 17:57:53 -0700

    minesweeper

--- react/day1/minesweeper/game.jsx
+++ react/day1/minesweeper/game.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import * as Minesweeper from './minesweeper';
 
 class Game extends React.Component {
   constructor(props){
     super(props);
+    let board = new Minesweeper.Board(9, 10);
+    this.state = { board };
   }
 
   render() {
