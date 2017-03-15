513a2b31d2175b0e4cfa4e30adfeab2efd594c8f
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-14 18:16:08 -0700

    minesweeper

---
+++ react/day1/minesweeper/frontend/board.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Board extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default Board; 
--- react/day1/minesweeper/frontend/game.jsx
+++ react/day1/minesweeper/frontend/game.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import * as Minesweeper from './minesweeper';
+import * as Minesweeper from '../minesweeper';
+import Board from './board';
 
 class Game extends React.Component {
   constructor(props){
     super(props);
     let board = new Minesweeper.Board(9, 10);
     this.state = { board };
+    this.updateGame = this.updateGame.bind(this);
+  }
+
+  updateGame() {
+
   }
 
   render() {
@@ -13,7 +19,7 @@ class Game extends React.Component {
     return(
       <div>
         "Hello World!!"
-
+        <Board board={this.state.board} updateGame={this.state.updateGame} />
       </div>
     );
   }
