0b14b84521f977817ea33a46856932d58304f285
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-14 17:31:59 -0700

    minesweeper

---
+++ react/day1/minesweeper/game.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Game extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+
+    return(
+      <div>
+        "Hello World!!"
+
+      </div>
+    );
+  }
+}
+
+export default Game;
