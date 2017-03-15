43c508dd00a5f5464f6e5c99d330611274a23086
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-14 18:48:55 -0700

    minesweeper

--- react/day1/minesweeper/frontend/board.jsx
+++ react/day1/minesweeper/frontend/board.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Tile from './tile';
 
 class Board extends React.Component {
   constructor(props){
@@ -14,4 +15,4 @@ class Board extends React.Component {
   }
 }
 
-export default Board; 
+export default Board;
