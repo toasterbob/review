701e6b856ffdd9ee493a63a01e2caf8c1740bc42
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-15 20:22:46 -0700

    minesweeper border

--- react/day1/minesweeper/frontend/tile.jsx
+++ react/day1/minesweeper/frontend/tile.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 class Tile extends React.Component {
   constructor(props){
     super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
 
   }
 
