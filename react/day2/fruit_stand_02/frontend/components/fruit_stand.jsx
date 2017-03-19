0cfb29260ddd05f2bf993cbae3241333517b698e
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-18 18:50:25 -0700

    css buttons

--- react/day2/fruit_stand_02/frontend/components/fruit_stand.jsx
+++ react/day2/fruit_stand_02/frontend/components/fruit_stand.jsx
@@ -7,11 +7,12 @@ const FruitStand = ({fruits, addApple, addOrange, addFruit, clearFruit}) => (
       {fruits.map((fruit, idx) => <li key={idx}>{fruit}</li>)}
     </ul>
 
-    <button onClick={addApple}>Apple</button>
-    <button onClick={addOrange}>Orange</button>
+    <button onClick={addApple}>Add Apple</button>
+    <button onClick={addOrange}>Add Orange</button>
+    <br/><br/>
     <AddFruit addFruit={addFruit}/>
     <br/>
-    <button onClick={clearFruit}>Clear</button>
+    <button onClick={clearFruit}>Clear All</button>
   </div>
 );
 
