1a2e5545c954c16672ebf1ba1df9f63c9b52701e
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-20 20:41:45 -0700

    Synthesizernotes and tones

--- react/day2/synthesizer2/frontend/synthesizer.jsx
+++ react/day2/synthesizer2/frontend/synthesizer.jsx
@@ -4,6 +4,6 @@ import Note from './util/note';
 
 document.addEventListener("DOMContentLoaded", function(){
   ReactDOM.render(<div>"Hello"</div>, document.getElementById('root'));
-});
+}); 
 
 window.Note = Note;
