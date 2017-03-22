07bfa4a5c6178b20f79f02f7687ee130ccf29c8e
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-21 23:20:04 -0700

    Synthesizer store

--- react/day2/synthesizer/frontend/synthesizer.jsx
+++ react/day2/synthesizer/frontend/synthesizer.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import Note from './util/note';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", function(){
+  const store = configureStore();
   ReactDOM.render(<div>"Hello"</div>, document.getElementById('root'));
 });
 
 window.Note = Note;
+window.store = store; 
