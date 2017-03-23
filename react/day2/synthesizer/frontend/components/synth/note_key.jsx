44ca090f69c37cbe4073a95afc5d83f06f556ac9
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-23 16:14:21 -0700

    note_key.jsx

---
+++ react/day2/synthesizer/frontend/components/synth/note_key.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class NoteKey extends React.Component {
+  constructor({note, pressed}){
+    super({note, pressed});
+
+  }
+
+  render(){
+
+    return(
+      <div>key</div>
+    );
+  }
+}
+
+export default NoteKey; 
