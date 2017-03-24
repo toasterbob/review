3a6e792cc3d208120e7abda46dbbee64a1316ba3
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-23 17:20:03 -0700

    key css

--- react/day2/synthesizer/frontend/components/synth/note_key.jsx
+++ react/day2/synthesizer/frontend/components/synth/note_key.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 class NoteKey extends React.Component {
-  constructor({note, pressed}){
-    super({note, pressed});
+  constructor({note, pressed, idx}){
+    super({note, pressed, idx});
     this.note = note;
     this.pressed = pressed;
+    this.idx = idx;
   }
 
   render(){
 
     return(
-      <div className={this.pressed ? "white-key-pressed" : "white-key"}>
-        <div className="note-name">{this.note}</div>
+      <div id={this.idx} className={this.pressed ? "white-key-pressed" : "white-key"}>
+        <div id={this.idx} className="note-name">{this.note}</div>
       </div>
     );
   }
