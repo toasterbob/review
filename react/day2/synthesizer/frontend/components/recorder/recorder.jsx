403c216818fb4d57179dd01029eb4b2cc361b2ee
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-27 13:51:38 -0700

    recorder component

--- react/day2/synthesizer/frontend/components/recorder/recorder.jsx
+++ react/day2/synthesizer/frontend/components/recorder/recorder.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 class Recorder extends React.Component {
+  constructor(props){
+    super(props);
+  }
 
+  render(){
+
+    return(
+      <div>Recorder</div>
+    );
+  }
 }
 
-export default Recorder; 
+export default Recorder;
