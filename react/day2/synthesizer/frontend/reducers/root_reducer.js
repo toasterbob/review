6b36c7b92e96b779d3776aa8cae9029360442538
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-26 21:35:34 -0700

    root reducer

--- react/day2/synthesizer/frontend/reducers/root_reducer.js
+++ react/day2/synthesizer/frontend/reducers/root_reducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 import notesReducer from './notes_reducer';
+import tracksReducer from './tracks_reducer';
+import isRecordingReducer from './is_recording_reducer';
 
 const rootReducer = combineReducers({
   notes: notesReducer
