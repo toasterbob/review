b885d9fe3bc3b09e7412eb8701070a03e73ebaff
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-27 14:00:24 -0700

    recorder container

--- react/day2/synthesizer/frontend/components/recorder/recorder_container.jsx
+++ react/day2/synthesizer/frontend/components/recorder/recorder_container.jsx
@@ -1,3 +1,13 @@
 import { connect } from 'react-redux';
 import Recorder from './recorder';
 import { startRecording, stopRecording, addNotes } from '../../actions/track_actions.js';
+
+const mapStateToProps = state => ({
+  tracks: state.tracks,
+  isRecordingReducer: state.isRecording
+});
+
+const mapDispatchToProps = dispatch => ({
+  startRecording: () => dispatch(startRecording()),
+  stopRecording: () => dispatch(stopRecording())
+});
