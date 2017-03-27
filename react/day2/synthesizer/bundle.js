18f3d9d4d8b8268bb7a4b09d44aa2add36b84982
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-27 15:06:52 -0700

    addNotes in Synth

--- react/day2/synthesizer/bundle.js
+++ react/day2/synthesizer/bundle.js
@@ -37848,72 +37848,49 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _react = __webpack_require__(12);
 
 var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var Recorder = function (_React$Component) {
-  _inherits(Recorder, _React$Component);
-
-  function Recorder(props) {
-    _classCallCheck(this, Recorder);
-
-    return _possibleConstructorReturn(this, (Recorder.__proto__ || Object.getPrototypeOf(Recorder)).call(this, props));
-  }
-
-  _createClass(Recorder, [{
-    key: "render",
-    value: function render() {
-      var startRecording = this.props.startRecording;
-      var stopRecording = this.props.stopRecording;
-      var isRecording = this.props.isReocrding;
-      return _react2.default.createElement(
+var Recorder = function Recorder(_ref) {
+  var startRecording = _ref.startRecording,
+      stopRecording = _ref.stopRecording,
+      isRecording = _ref.isRecording;
+  return _react2.default.createElement(
+    "div",
+    { className: "recorder" },
+    _react2.default.createElement("br", null),
+    _react2.default.createElement(
+      "h2",
+      null,
+      "Record your song!"
+    ),
+    _react2.default.createElement(
+      "div",
+      { className: "buttons" },
+      _react2.default.createElement(
         "div",
-        { className: "recorder" },
-        _react2.default.createElement("br", null),
+        null,
         _react2.default.createElement(
-          "h2",
-          null,
-          "Record your song!"
-        ),
-        _react2.default.createElement(
-          "div",
-          { className: "buttons" },
-          _react2.default.createElement(
-            "div",
-            null,
-            _react2.default.createElement(
-              "button",
-              { onClick: startRecording, disabled: isRecording },
-              "start"
-            )
-          ),
-          _react2.default.createElement(
-            "div",
-            null,
-            _react2.default.createElement(
-              "button",
-              { onClick: stopRecording, disabled: !isRecording },
-              "stop"
-            )
-          )
+          "button",
+          { onClick: startRecording, disabled: isRecording },
+          "start"
         )
-      );
-    }
-  }]);
-
-  return Recorder;
-}(_react2.default.Component);
+      ),
+      _react2.default.createElement(
+        "div",
+        null,
+        _react2.default.createElement(
+          "button",
+          { onClick: stopRecording, disabled: !isRecording },
+          "stop"
+        )
+      )
+    )
+  );
+};
 
 exports.default = Recorder;
 
--- react/day2/synthesizer/frontend/components/recorder/recorder.jsx
+++ react/day2/synthesizer/frontend/components/recorder/recorder.jsx
@@ -1,29 +1,19 @@
 import React from "react";
 
-class Recorder extends React.Component {
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-    let startRecording = this.props.startRecording;
-    let stopRecording = this.props.stopRecording;
-    let isRecording = this.props.isReocrding; 
-    return(
-      <div className="recorder">
-        <br/>
-        <h2>Record your song!</h2>
-        <div className="buttons">
-          <div>
-            <button onClick={startRecording} disabled={isRecording }>start</button>
-          </div>
-          <div>
-            <button onClick={stopRecording} disabled={!isRecording }>stop</button>
-          </div>
-        </div>
+const Recorder = ({ startRecording, stopRecording, isRecording}) => (
+  <div className="recorder">
+    <br/>
+    <h2>Record your song!</h2>
+    <div className="buttons">
+      <div>
+        <button onClick={startRecording} disabled={isRecording }>start</button>
       </div>
-    );
-  }
-}
+      <div>
+        <button onClick={stopRecording} disabled={!isRecording }>stop</button>
+      </div>
+    </div>
+  </div>
+);
+
 
 export default Recorder;
