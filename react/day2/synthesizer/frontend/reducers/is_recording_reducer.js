e2837bd033d30a28294895ef85186c57a8503492
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-26 20:49:32 -0700

    recording reducer

--- react/day2/synthesizer/frontend/reducers/is_recording_reducer.js
+++ react/day2/synthesizer/frontend/reducers/is_recording_reducer.js
@@ -1,7 +1,15 @@
 import { START_RECORDING, STOP_RECORDING, ADD_NOTES } from '../actions/tracks_actions';
 
-const isRecordingReducer = (state = [], action) => {
-
+const isRecordingReducer = (state = false, action) => {
+  Object.freeze(state);
+  switch(action.type){
+    case START_RECORDING:
+      return true;
+    case STOP_RECORDING:
+      return false;
+    default:
+    return state;
+  }
 };
 
 export default isRecordingReducer;
