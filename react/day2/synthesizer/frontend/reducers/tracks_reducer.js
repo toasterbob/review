38b468cf52e1b7e7e4a03dca763d64dffabc6046
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-26 21:26:17 -0700

    tracks reducer

--- react/day2/synthesizer/frontend/reducers/tracks_reducer.js
+++ react/day2/synthesizer/frontend/reducers/tracks_reducer.js
@@ -1,13 +1,20 @@
 import { START_RECORDING, STOP_RECORDING, ADD_NOTES } from '../actions/tracks_actions';
 import { merge } from 'lodash/merge';
 
-let currTrackId = 0; 
+let currTrackId = 0;
 
 
 const tracksReducer = (state = [], action) => {
   Object.freeze(state);
   switch(action.type){
     case START_RECORDING:
+      currTrackId++;
+      return merge ({}, state, {
+        id: currTrackId,
+        name: `Track ${currTrackId}`,
+        roll: [],
+        timeStart: action.timeNow
+      });
     case STOP_RECORDING:
     case ADD_NOTES:
     default:
