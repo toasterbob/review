62a824530d5886b55c1fb9ba0aef9a9e2abd44d3
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-28 15:51:56 -0700

    track.jsx

--- react/day2/synthesizer/frontend/components/jukebox/track.jsx
+++ react/day2/synthesizer/frontend/components/jukebox/track.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Track = state => (
-  <div>Track &nbsp; <button>play</button></div>
+const Track = ({isRecording, isPlaying, onPlay}) => (
+  <div>Track &nbsp; <button onClick={onPlay(track)} disabled={isRecording || isPlaying }>play</button></div>
 );
 
 export default Track;
