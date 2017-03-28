e642b815e1a225ddd7b5489d1144ca455c935bb1
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-28 10:03:38 -0700

    playback method

--- react/day2/synthesizer/frontend/components/jukebox/jukebox_container.jsx
+++ react/day2/synthesizer/frontend/components/jukebox/jukebox_container.jsx
@@ -12,6 +12,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   onPlay: track => e => {
     dispatch(startPlaying());
+    let playBackStartTime = Date.now(); 
   }
 });
 
