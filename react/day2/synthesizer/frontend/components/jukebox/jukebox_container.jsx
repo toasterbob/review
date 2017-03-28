ccc22bcdf8b29d6977e6038eefedace8f9706069
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-27 20:20:15 -0700

    jukebox container

--- react/day2/synthesizer/frontend/components/jukebox/jukebox_container.jsx
+++ react/day2/synthesizer/frontend/components/jukebox/jukebox_container.jsx
@@ -2,3 +2,20 @@ import { connect } from 'react-redux';
 import { startPlaying, stopPlaying} from '../../actions/playing_actions';
 import { groupUpdate } from '../../actions/notes_actions';
 import Jukebox from './jukebox';
+
+const mapStateToProps = state => ({
+  tracks: state.tracks,
+  isRecording: state.isRecording,
+  isPlaying: state.isPlaying
+});
+
+const mapDispatchToProps = dispatch => ({
+  onPlay: track => e => {
+    dispatch(startPlaying());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Jukebox);
