917679e8b8ed4cbeccd44c8147bc316f21945e3c
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-27 20:06:31 -0700

    jukebox container

---
+++ react/day2/synthesizer/frontend/components/jukebox/jukebox_container.jsx
@@ -0,0 +1,4 @@
+import { connect } from 'react-redux';
+import { startPlaying, stopPlaying} from '../../actions/playing_actions';
+import { groupUpdate } from '../../actions/notes_actions';
+import Jukebox from './jukebox';
