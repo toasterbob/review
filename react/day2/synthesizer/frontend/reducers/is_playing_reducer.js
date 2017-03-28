0c3496b0f0a2ec6dbf7d6702f03250cfbd2919a3
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-27 19:11:47 -0700

    isPlayingReducer

---
+++ react/day2/synthesizer/frontend/reducers/is_playing_reducer.js
@@ -0,0 +1,15 @@
+import { START_PLAYING, STOP_PLAYING } from '../actions/playing_actions';
+
+const isPlayingReducer = (state = false, action) => {
+  Object.freeze(state);
+  switch(action.type){
+    case START_PLAYING:
+      return true;
+    case STOP_PLAYING:
+      return false; 
+    default:
+      return state;
+  }
+};
+
+export default isPlayingReducer;
