4e715f04da08aab90522434bbae04377b13d239f
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-28 17:12:16 -0700

    tracks

--- react/day2/synthesizer/frontend/components/jukebox/track.jsx
+++ react/day2/synthesizer/frontend/components/jukebox/track.jsx
@@ -3,5 +3,5 @@ import React from 'react';
 const Track = ({track, onPlay, disabled}) => (
   <div className='track' key={track.id}>{track.name} &nbsp; <button onClick={onPlay} disabled={disabled}>play</button></div>
 );
-
+ 
 export default Track;
