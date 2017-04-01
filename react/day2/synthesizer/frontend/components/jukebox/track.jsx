3e0c3a427c830ad8593c8260003a4e025843c998
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-01 15:38:15 -0700

    playback button css

--- react/day2/synthesizer/frontend/components/jukebox/track.jsx
+++ react/day2/synthesizer/frontend/components/jukebox/track.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Track = ({track, onPlay, disabled}) => (
   <div className='track' key={track.id}>
     {track.name} &nbsp; <button onClick={onPlay} disabled={disabled}>play</button>
-
   </div>
 );
 
