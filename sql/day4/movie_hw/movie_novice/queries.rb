7960d95e879a1afc525794b31ea14ab1c953972f
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-02 22:12:17 -0700

    movie

--- sql/day4/movie_hw/movie_novice/queries.rb
+++ sql/day4/movie_hw/movie_novice/queries.rb
@@ -29,7 +29,8 @@ end
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-
+  Movie.select(title:)
+       .where(score: >= 9)
 end
 
 def star_wars
