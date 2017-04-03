b560f45db08b8b9bf04e2eee45d1f1ce2a11f5c5
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-02 22:25:32 -0700

    movie

--- sql/day4/movie_hw/movie_novice/queries.rb
+++ sql/day4/movie_hw/movie_novice/queries.rb
@@ -48,7 +48,11 @@ def below_average_years
   #with the count of movies scoring under 5 aliased as bad_movies,
   #in descending order
   # hint: use 'select', 'where', 'group', 'order'
-
+  Movie
+    .select("yr, COUNT(*) AS bad_movies")
+    .where("score < 5")
+    .group(:yr)
+    .order("bad_movies DESC")
 end
 
 def alphabetized_actors
