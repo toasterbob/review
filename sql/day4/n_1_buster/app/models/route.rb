a7d799135878bb1f2c7ddce58a2d0995df410491
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-03 20:38:08 -0700

    n+1

--- sql/day4/n_1_buster/app/models/route.rb
+++ sql/day4/n_1_buster/app/models/route.rb
@@ -22,6 +22,17 @@ class Route < ActiveRecord::Base
   end
 
   def better_drivers_query
-    # TODO: your code here
+    buses = self.buses.includes(:drivers)
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
   end
 end
