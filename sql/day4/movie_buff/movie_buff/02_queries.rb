ef6338b26a95b2a7c5abd7103c62c9380ed747a9
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-04 18:07:39 -0700

    movie buff queries 1

--- sql/day4/movie_buff/movie_buff/02_queries.rb
+++ sql/day4/movie_buff/movie_buff/02_queries.rb
@@ -1,18 +1,27 @@
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where(yr: 1980..1989, score: 3..5)
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie
+    .group(:yr)
+    .having("MAX(score) < 8")
+    .pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-
+  Actor
+    .select(:id, :name)
+    .joins(:movies)
+    .where(movies: {title: title})
+    .order("castings.ord")
 end
 
 def vanity_projects
@@ -20,7 +29,7 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
-
+  
 end
 
 def most_supportive
