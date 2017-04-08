9a00d7514e66300e02ae1ae8bf9911831a6d088d
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-07 22:04:10 -0700

    poll app

--- sql/day4/polls/PollApp/db/migrate/20170408045820_create_polls.rb
+++ sql/day4/polls/PollApp/db/migrate/20170408045820_create_polls.rb
@@ -2,7 +2,7 @@ class CreatePolls < ActiveRecord::Migration[5.0]
   def change
     create_table :polls do |t|
       t.string :title
-      t.string :author_id
+      t.integer :author_id
       t.timestamps
     end
   end
--- sql/day4/polls/PollApp/db/schema.rb
+++ sql/day4/polls/PollApp/db/schema.rb
@@ -10,14 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408045820) do
+ActiveRecord::Schema.define(version: 20170408050229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "polls", force: :cascade do |t|
     t.string   "title"
-    t.string   "author_id"
+    t.integer  "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "poll_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
