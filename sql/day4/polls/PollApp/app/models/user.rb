37b4d81172334c9c5a1270b50519b552fcb01a21
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-07 22:17:47 -0700

    poll app

--- sql/day4/polls/PollApp/app/models/user.rb
+++ sql/day4/polls/PollApp/app/models/user.rb
@@ -3,6 +3,11 @@ class User
   has_many :authored_polls,
   primary_key: :id,
   foreign_key: :poll_id,
-  class_name: Poll 
+  class_name: Poll
+
+  has_many :responses,
+  primary_key: :id,
+  foreign_key: :user_id,
+  class_name: Response 
 
 end
