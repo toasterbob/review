55d2246b418716c559a5442ed9beb5cb063438d8
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-12 16:44:15 -0700

    poll model

--- sql/day4/polls/Polling/app/models/poll.rb
+++ sql/day4/polls/Polling/app/models/poll.rb
@@ -2,5 +2,10 @@ class Poll < ActiveRecord::Base
   belongs_to :author,
   primary_key: :id,
   foreign_key: :author_id,
-  class_name: :User 
+  class_name: :User
+
+  has_many :questions,
+  primary_key: :id,
+  foreign_key: :poll_id,
+  class_name: :Question 
 end
--- sql/day4/polls/Polling/app/models/question.rb
+++ sql/day4/polls/Polling/app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ActiveRecord::Base
-  
+  belongs_to :poll,
+  primary_key: :id,
+  foreign_key: :poll_id,
+  class_name: :Poll
 end
