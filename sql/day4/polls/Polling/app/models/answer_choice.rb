5b1e1084c3b5adbd253a54212d7b4fcfdbe0f80d
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-12 17:20:38 -0700

    response model

--- sql/day4/polls/Polling/app/models/answer_choice.rb
+++ sql/day4/polls/Polling/app/models/answer_choice.rb
@@ -1,5 +1,5 @@
 class AnswerChoice < ActiveRecord::Base
-  belongs_to: question,
+  belongs_to :question,
   primary_key: :id,
   foreign_key: :question_id,
   class_name: :Question
---
+++ sql/day4/polls/Polling/db/migrate/20170413001805_create_responses.rb
@@ -0,0 +1,9 @@
+class CreateResponses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :responses do |t|
+      t.integer :question_id
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
