dd246814a239c61637309708e560e226af890b2d
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-12 17:35:26 -0700

    seed data

--- sql/day4/polls/Polling/db/seeds.rb
+++ sql/day4/polls/Polling/db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do
+  u1 = User.create!(user_name: "Bobby Tables")
+  u2 = User.create!(user_name: "Julia Childs")
+
+  p1 = Poll.create!(title: "Pony Boy Eats a Ship", author_id: u1.id)
+
+  q1 = Question.create!(text: "Is the bonga bonga the goldfish king?", poll_id: p1.id)
+  ac1 = AnswerChoice.create!(text: "Jeep Cherokee", question_id: q1.id)
+  ac2 = AnswerChoice.create!(text: "Energy equals mass times the speed of light", question_id: q1.id)
+  ac3 = AnswerChoice.create!(text: "RON!", question_id: q1.id)
+
+  q2 = Question.create!(text: "Why did I run into a pole?", poll_id: p1.id)
+  ac4 = AnswerChoice.create!(text: "Too many drugs", question_id: q2.id)
+  ac5 = AnswerChoice.create!(text: "Challenge", question_id: q2.id)
+  ac6 = AnswerChoice.create!(text: "Birdy, birdy in the SKY", question_id: q2.id)
+
+  r1 = Response.create!(
+    respondent_id: u2.id, answer_choice_id: ac2.id
+  )
+  r2 = Response.create!(
+    respondent_id: u2.id, answer_choice_id: ac4.id
+  )
+end
