775dd71809ae211bcee3a8dea985de4677d95527
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-12 17:22:33 -0700

    response model

--- sql/day4/polls/Polling/app/models/response.rb
+++ sql/day4/polls/Polling/app/models/response.rb
@@ -1,3 +1,8 @@
 class Response < ActiveRecord::Base
+  belongs_to :user,
+  primary_key: :id,
+  foreign_key: :user_id,
+  class_name: :User
 
-end 
+
+end
--- sql/day4/polls/Polling/app/models/user.rb
+++ sql/day4/polls/Polling/app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
   foreign_key: :author_id,
   class_name: :Poll
 
-  
+  belongs_to :responses,
+  primary_key: :id,
+  foreign_key: :user_id,
+  class_name: :Response
 end
