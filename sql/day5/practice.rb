e233a41def3e48bb7ee1aa2807a487a2b3047668
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-14 14:21:35 -0700

    practice

---
+++ sql/day5/practice.rb
@@ -0,0 +1,20 @@
+class Dog
+  # defines a class method that will define more methods; this is
+  # called a **macro**.
+
+  def self.makes_sound(name)
+    define_method(name) { puts "#{name}!" }
+  end
+
+  makes_sound(:woof)
+  makes_sound(:bark)
+  makes_sound(:grr)
+end
+
+dog = Dog.new
+dog.woof
+dog.bark
+dog.grr
+
+Dog.makes_sound(:bow_wow)
+dog.bow_wow
