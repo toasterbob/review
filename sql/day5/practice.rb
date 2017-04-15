a9cc4d0453b266d8331bfa4695a8ca091195b965
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-14 20:25:54 -0700

    practice

--- sql/day5/practice.rb
+++ sql/day5/practice.rb
@@ -35,3 +35,26 @@ end
 T.new.adfasdfa(:a, :b, :c)
 
 T.new.hello
+
+class Cat
+  def say(anything)
+    puts anything
+  end
+
+  def method_missing(method_name)
+    method_name = method_name.to_s
+    if method_name.start_with?("say_")
+      text = method_name[("say_".length)..-1]
+
+      say(text)
+    else
+      # do the usual thing when a method is missing (i.e., raise an
+      # error)
+      super
+    end
+  end
+end
+
+earl = Cat.new
+earl.say_hello # puts "hello"
+earl.say_goodbye # puts "goodbye"
