bc4cc7dcbd69bc6b6fb7678f9f700ceb5f936358
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-16 13:44:07 -0700

    practice

--- challenges/practice.rb
+++ challenges/practice.rb
@@ -1,5 +1,17 @@
 
+def StairCase(n)
+ stairs = 1
+ while stairs <= n
+  spaces = n - stairs
+     puts "#{" " * spaces}#{"#" * stairs}"
+  stairs += 1
+ end
+end
+
 
 if __FILE__ == $PROGRAM_NAME
-  
+
 end
+
+#best asymptotic runtime complexity
+#heap sort, bubble sort, selection sort, insertion sort
