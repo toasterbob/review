636dc5b035c662440f40511b920a9cf071453eec
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-15 23:50:56 -0700

    metacorgis metasnacks

--- sql/day5/metacorgis/meta_corgis.rb
+++ sql/day5/metacorgis/meta_corgis.rb
@@ -111,7 +111,7 @@ class MetaCorgiSnacks
 
   def method_missing(name, *args)
     meta_data = "@snack_box[1][#{name}]"
-    data2 = @snack_box.send(meta_data)
+    data2 = send(meta_data)
     result = "Treat: #{data2.info}: #{data2.tastiness} "
     data2.tastiness > 30 ? "* #{result}" : result
   end
