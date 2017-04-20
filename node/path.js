404cedb8ce076ad1df007c4573483ba932d41d3d
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-20 14:06:41 -0700

    path.js

--- node/path.js
+++ node/path.js
@@ -5,3 +5,5 @@ console.log(path.normalize('test/test1//2slashes/1slash/tab/..'));
 
 // Join multiple paths together
 console.log(path.join('/first', 'second', 'something', 'then', '..'));
+
+// Resolve a path (find the absolute path)
