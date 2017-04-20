1d156be3266504d859738710ffcb2d13e5ba6ecf
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-20 14:06:04 -0700

    path.js

--- node/path.js
+++ node/path.js
@@ -2,3 +2,6 @@ var path = require("path");
 
 // Normalize a path
 console.log(path.normalize('test/test1//2slashes/1slash/tab/..'));
+
+// Join multiple paths together
+console.log(path.join('/first', 'second', 'something', 'then', '..'));
