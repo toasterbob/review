7c154fca2001afaac060dae3c3527ee614f56a75
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-20 11:42:42 -0700

    evenMoreHelpers.js

--- node/main.js
+++ node/main.js
@@ -1,9 +1,12 @@
 var helpers = require('./helpers');
 // import * as helpers from './helpers';
 var moreHelpers = require('./moreHelpers');
-
+var evenMoreHelpers = require('./evenMoreHelpers');
 
 helpers.sayHi();
+
 console.log(moreHelpers.firstName);
 moreHelpers.sayHello();
 moreHelpers.sayGoodbye();
+
+evenMoreHelpers();  //this is a function on its own 
