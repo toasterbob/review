41f64ef93c3493916d75d94b488f66ca2dc33d42
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-30 22:27:20 -0700

    hamming

--- challenges/js/leetcode/hamming.js
+++ challenges/js/leetcode/hamming.js
@@ -1,8 +1,17 @@
 var hammingDistance = function(x, y) {
-    x = x.toString(2);
-    y = y.toString(2);
+
+    function get_bits(value){
+        var base2_ = (value).toString(2).split("").reverse().join("");
+        var baseL_ = new Array(32 - base2_.length).join("0");
+        var base2 = base2_ + baseL_;
+        return base2;
+    }
+
+    x = get_bits(x);
+    y = get_bits(y);
+
     let ham = 0;
-    for(let i = 0; i < x.length; i++){
+    for(let i = 0; i < y.length; i++){
         if(x[i] !== y[i]){
             ham++;
         }
