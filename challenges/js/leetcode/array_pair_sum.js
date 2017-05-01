142f69cf066a1785132bd3b7e8229ba387e84d5e
Mark Noizumi <mark.noizumi@gmail.com>
2017-04-30 22:04:03 -0700

    array pair sum

--- challenges/js/leetcode/array_pair_sum.js
+++ challenges/js/leetcode/array_pair_sum.js
@@ -2,11 +2,20 @@ var arrayPairSum = function(nums) {
     function sortNumber(a,b) {
         return a - b;
     }
-    nums = nums.sort(sortNumber);
+    function sortNumber2(a,b){
+        if(a > b){
+            return 1;
+        } else if(a < b){
+            return -1;
+        } else{
+            return 0;
+        }
+    }
+    nums = nums.sort(sortNumber2);
     let sum = 0;
     for(let i = 0; i < nums.length; i++){
         if(i % 2 === 0){
-            sum += nums[i];
+            sum += nums[i]
         }
     }
     return sum;
