14ea71041c3b8536eaec660617ec00271852be6a
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-01 22:46:49 -0700

    ten

--- challenges/js/coderbyte/ten.js
+++ challenges/js/coderbyte/ten.js
@@ -24,3 +24,15 @@ function multiply(a){
 
 let d = multiply(5)(6);
 d(2) //60
+
+// Number 4 - bind
+
+function fullName() {
+  return "Hello, this is " + this.first + " " + this.last;
+}
+
+console.log(fullName()); // => Hello this is undefined undefined
+
+// create a person object and pass its values to the fullName function
+var person = {first: "Foo", last: "Bar"};
+console.log(fullName.bind(person)()); // => Hello this is Foo Bar
