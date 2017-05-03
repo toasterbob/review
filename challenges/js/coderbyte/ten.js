f17f937d86b986e467a5fd2aa24b96268827736a
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-02 17:50:43 -0700

    ten

--- challenges/js/coderbyte/ten.js
+++ challenges/js/coderbyte/ten.js
@@ -102,3 +102,12 @@ modifyArray(arr, function() {
 });
 
 //9
+a = 0.1 + 0.2 //0.30000000000000004
+
+0.1 + 0.2 === 0.3 // false
+// This will surprisingly output false because of floating point errors
+// in internally representing certain numbers. 0.1 + 0.2 does not nicely
+// come out to 0.3 but instead the result is actually 0.30000000000000004
+// because the computer cannot internally represent the correct number.
+// One solution to get around this problem is to round the results when
+// doing arithmetic with decimal numbers.
