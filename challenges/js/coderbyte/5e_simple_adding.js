a75a32f0e1eeaf2b322b684204f8fa29adc556bd
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-02 22:06:45 -0700

    simple adding

--- challenges/js/coderbyte/5e_simple_adding.js
+++ challenges/js/coderbyte/5e_simple_adding.js
@@ -1,9 +1,32 @@
-function SimpleAdding(num) { 
+function SimpleAdding(num) {
 
   let sum = 0;
   for(let i = 1; i <= num; i++){
       sum += i;
   }
   return sum;
+
+}
+
+function SimpleAdding2(num) {
+
+  let sum = 0;
+
+  for(let i = 1; i <= num; i++) sum += i;
+
+  return sum;
+
+}
+
+//recursive
+
+function SimpleAdding(num) {
+
+  if(num === 1){
+      return 1;
+  } else {
+      return num + SimpleAdding(num - 1);
+  }
+
 
 }
