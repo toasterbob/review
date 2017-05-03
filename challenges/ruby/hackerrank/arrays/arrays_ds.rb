9dbc6b8179bce237ae6759e070e2883ada2e8174
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-03 12:58:52 -0700

    arr

--- challenges/ruby/hackerrank/arrays/arrays_ds.rb
+++ challenges/ruby/hackerrank/arrays/arrays_ds.rb
@@ -5,3 +5,12 @@ arr = gets.strip
 arr = arr.split(' ').map(&:to_i)
 
 puts arr.reverse.join(" ")
+
+
+#!/bin/ruby
+
+n = gets.strip.to_i
+arr = gets.strip
+arr = arr.split(' ').map(&:to_i)
+
+puts arr.reduce(0,:+);
