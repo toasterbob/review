7dcc5303fcb200ff99d939ae97f39389ee88b1ad
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-02 17:45:16 -0700

    ten

--- challenges/js/coderbyte/ten.js
+++ challenges/js/coderbyte/ten.js
@@ -84,3 +84,21 @@ var avg = arr.average();
 console.log(avg);
 
 // 8
+// A callback function is a function that is passed to another function
+// as an argument and is executed after some operation has been completed.
+// Below is an example of a simple callback function that logs to the
+// console after some operations have been completed.
+function modifyArray(arr, callback) {
+  // do something to arr here
+  arr.push(100);
+  // then execute the callback function that was passed
+  callback();
+}
+
+var arr = [1, 2, 3, 4, 5];
+
+modifyArray(arr, function() {
+  console.log("array has been modified", arr);
+});
+
+//9
