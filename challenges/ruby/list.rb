fdaa2aaab5c32de6ff21b8e37c148bd587ed1cd3
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-03 16:38:46 -0700

    plus minus

--- challenges/ruby/list.rb
+++ challenges/ruby/list.rb
@@ -1,1 +1,5 @@
-puts '%.6f' % (1/2) # 0.500000
+puts '%.6f' % (1.0/2) # 0.500000
+
+negative, rest = [0, -1, 2, 3, -9].partition{|n| n < 0 } #[[-1, -9], [0, 2, 3]]
+positive, zero = rest.partition{|n| n > 0} #
+p negative, positive, zero
