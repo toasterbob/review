3ea99a7a18eec2c1a7e53ae7f6c8ba9e1ed38f9b
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-02 22:24:07 -0700

    letter capitalize

--- challenges/js/list.js
+++ challenges/js/list.js
@@ -1,4 +1,6 @@
 ("a").charCodeAt(); // 97
 String.fromCharCode(122); // "z"
 
-result += (word[0].toUpperCase() + word.substr(1, word.length - 1) + " ");
+//capitalize with substrings 
+let word = "mark";
+let result += (word[0].toUpperCase() + word.substr(1, word.length - 1) + " ");
