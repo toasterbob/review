6186d39271163b7ce083420ffd99ce4811e3c3ba
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-04 15:24:30 -0700

    challenge

---
+++ challenges/js/hackerRank/challenge_i/c1.rb
@@ -0,0 +1,12 @@
+def countHoles(num)
+    holes = 0
+    no_holes = [1, 2, 3, 5, 7]
+    one_hole = [0, 4, 6, 9]
+    two_holes = [8]
+
+    num.to_s.split("").each do |number|
+        holes += 1 if one_hole.include?(number.to_i)
+        holes += 2 if number == "8"
+    end
+    holes
+end 
