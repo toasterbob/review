b659b45fa278c95fb53ed12db7c70165845c9a40
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-06 00:36:16 +0530

    react tic-tac-toe

--- react/tic-tac-toe/ttt.js
+++ react/tic-tac-toe/ttt.js
@@ -1,7 +1,7 @@
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert(this.props.value)}>
         {this.props.value}
       </button>
     );
