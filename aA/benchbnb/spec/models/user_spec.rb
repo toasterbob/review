11a4cb57680d2a70f57a9043e90cb87222796226
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-05 17:35:19 -0700

    rspec user model

--- aA/benchbnb/spec/models/user_spec.rb
+++ aA/benchbnb/spec/models/user_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
-end
 
-RSpec.describe User, type: :model do
   describe 'validations' do
     # validation tests go here
   end
@@ -12,4 +10,5 @@ RSpec.describe User, type: :model do
   describe 'associations' do
     # association tests go here
   end
+
 end
