d33d6a6a37a232816b7e6535f5e13817fd5c157c
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-06 17:21:22 -0700

    rspec user model

--- aA/benchbnb/spec/models/user_spec.rb
+++ aA/benchbnb/spec/models/user_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:username) }
-    it { should validate_uniqueness_of(:username) } 
+    it { should validate_uniqueness_of(:username) }
   end
 
   describe 'associations' do
