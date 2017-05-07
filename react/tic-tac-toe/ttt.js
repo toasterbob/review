3a47bb3365919e403b7c9f0a966aa3b2ae3c84ee
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-07 06:27:52 +0530

    react tic-tac-toe

--- react/tic-tac-toe/ttt.js
+++ react/tic-tac-toe/ttt.js
@@ -1,16 +1,28 @@
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={() => alert("You clicked " + this.props.value)}>
-        {this.props.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+         {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+      );
   }
 
   render() {
