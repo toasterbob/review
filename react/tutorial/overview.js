74c2800f8d61cb86518841eb2993cc10721e8853
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-07 06:19:16 +0530

    react tic-tac-toe

--- react/tutorial/overview.js
+++ react/tutorial/overview.js
@@ -41,5 +41,33 @@ class ShoppingList extends React.Component {
    }
  }
  // This uses the new JavaScript arrow function syntax. Note that we're passing
- // a function as the onClick prop. 
+ // a function as the onClick prop.
 // Doing onClick={alert('click')} would alert immediately instead of when the button is clicked.
+
+constructor() {
+    super();
+    this.state = {
+      value: null,
+    };
+  }
+// In JavaScript classes, you need to explicitly call super(); when
+// defining the constructor of a subclass.
+class Square extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      value: null,
+    };
+  }
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+         {this.state.value}
+      </button>
+    );
+  }
+}
+// Whenever this.setState is called, an update to the component is scheduled, 
+// causing React to merge in the passed state update and rerender the
+// component along with its descendants. When the component rerenders,
+// this.state.value will be 'X' so you'll see an X in the grid.
