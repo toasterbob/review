49fa5dd2a3f4dbfe8ace8979a1cfb4ce46c83bbf
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-08 16:37:47 -0700

    map into numbers

--- challenges/js/amne/amne.js
+++ challenges/js/amne/amne.js
@@ -2,8 +2,8 @@ let first = "5 3";
 let range = "188930 194123 201345 154243 154243";
 
 const priceRange = function(first, range){
-  let nums = first.split(" ");
-  let arr = range.split(" ");
+  let nums = first.split(" ").map(a => { return parseInt(a)});
+  let arr = range.split(" ").map(a => { return parseInt(a)});
   console.log(nums);
   console.log(arr);
 }
