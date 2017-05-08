deacc6da5a9d356f828b1b7316c4a52e06cb2c6b
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-08 16:33:09 -0700

    amne.js

---
+++ challenges/js/amne/amne.js
@@ -0,0 +1,12 @@
+let first = "5 3";
+let range = "188930 194123 201345 154243 154243";
+
+const priceRange = function(first, range){
+  let nums = first.split(" ");
+  let arr = range.split(" ");
+  console.log(nums);
+  console.log(arr);
+}
+
+
+priceRange(first, range);
