229630e65a5c78c95676cc1b82d04cc14f16858f
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-08 21:39:01 -0700

    basic logic

--- challenges/js/amne/amne.js
+++ challenges/js/amne/amne.js
@@ -1,30 +1,31 @@
-let first = "5 3";
-let range = "188930 194123 201345 154243 154243";
 
-const priceRange = function(first, range){
-  let nums = first.split(" ").map(a => { return parseInt(a)});
-  let arr = range.split(" ").map(a => { return parseInt(a)});
-  let results = []
+const priceRange = function(){
+  let first = "5 3";
+  let range = "188930 194123 201345 154243 154243";
+
+  let nums = first.split(" ").map(a => { return parseInt(a);});
+  let arr = range.split(" ").map(a => { return parseInt(a);});
+  let results = [];
 
   for(let i = 0; i <= nums[0] - nums[1]; i++){
     let total = 0;
-    let decrease_streak = 0;
-    let increase_streak = 0;
-    
+    let decreaseStreak = 0;
+    let increaseStreak = 0;
+
     for(let j = i; j < nums[1] + i - 1; j++){
       if(arr[j] < arr[j+1]){
-        decrease_streak = 0;
-        total = total + (1 + increase_streak)
-        increase_streak++
+        decreaseStreak = 0;
+        total = total + (1 + increaseStreak);
+        increaseStreak++;
       } else if (arr[j] > arr[j+1]){
-        increase_streak = 0
-        total = total - (1 + decrease_streak)
-        decrease_streak++;
+        increaseStreak = 0;
+        total = total - (1 + decreaseStreak);
+        decreaseStreak++;
       }
     }
     console.log(total);
   }
-}
+};
 
 
-priceRange(first, range);
+priceRange();
