f419a8d6bed0042fc768f5404ca474129a4d96a4
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-08 21:32:22 -0700

    basic logic

--- challenges/js/amne/amne.js
+++ challenges/js/amne/amne.js
@@ -4,20 +4,22 @@ let range = "188930 194123 201345 154243 154243";
 const priceRange = function(first, range){
   let nums = first.split(" ").map(a => { return parseInt(a)});
   let arr = range.split(" ").map(a => { return parseInt(a)});
-  console.log(nums);
-  console.log(arr);
+  let results = []
+
   for(let i = 0; i <= nums[0] - nums[1]; i++){
     let total = 0;
-
+    let decrease_streak = 0;
+    let increase_streak = 0;
+    
     for(let j = i; j < nums[1] + i - 1; j++){
       if(arr[j] < arr[j+1]){
         decrease_streak = 0;
-        total += (1 + increase_streak)
-        increase_streak += 1;
+        total = total + (1 + increase_streak)
+        increase_streak++
       } else if (arr[j] > arr[j+1]){
         increase_streak = 0
-        total -= (1 + decrease_streak)
-        decrease_streak += 1;
+        total = total - (1 + decrease_streak)
+        decrease_streak++;
       }
     }
     console.log(total);
