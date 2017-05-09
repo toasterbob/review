e5444a5eb930f57b32499277f92d915de3ec7c94
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-09 16:37:21 -0700

    process

---
+++ node/args.js
@@ -0,0 +1,11 @@
+console.log(process.argv.slice(2));
+
+// node args.js 1 2 3
+//The first value refers to the command which we are using to run node,
+// the second is the absolute path to our file
+
+// [ '/Users/marknoizumi/.nvm/versions/node/v6.2.1/bin/node',
+//   '/Users/marknoizumi/Desktop/curriculum/node/args.js',
+//   '1',
+//   '2',
+//   '3' ]
