9f0e02f9abe94dcafd66166e81f995facc5a2a16
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-09 16:40:54 -0700

    process

--- node/args.js
+++ node/args.js
@@ -1,4 +1,4 @@
-console.log(process.argv.slice(2));
+console.log(process.argv);
 
 // node args.js 1 2 3
 //The first value refers to the command which we are using to run node,
@@ -7,5 +7,10 @@ console.log(process.argv.slice(2));
 // [ '/Users/marknoizumi/.nvm/versions/node/v6.2.1/bin/node',
 //   '/Users/marknoizumi/Desktop/curriculum/node/args.js',
 //   '1',
+//   '2',
+//   '3' ]
+
+console.log(process.argv.slice(2));
+// [ '1',
 //   '2',
 //   '3' ]
