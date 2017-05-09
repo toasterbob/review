7bad5aa3dec9d3ba92f44e29ab7326c256166624
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-09 16:30:11 -0700

    path.js

--- node/path.js
+++ node/path.js
@@ -1,13 +1,13 @@
 var path = require("path");
 
 // Normalize a path
-console.log(path.normalize('test/test1//2slashes/1slash/tab/..'));
+console.log(path.normalize('test/test1//2slashes/1slash/tab/..')); // /test/test1/2slashes/1slash
 
 // Join multiple paths together
-console.log(path.join('/first', 'second', 'something', 'then', '..'));
+console.log(path.join('/first', 'second', 'something', 'then', '..')); // /first/second/something
 
 // Resolve a path (find the absolute path)
-console.log(path.resolve('first.js'));
+console.log(path.resolve('first.js')); ///Users/marknoizumi/Desktop/curriculum/node/first.js
 
 // Find the extention of a filename
 console.log(path.extname('main.js'));
