e5e40c8a38313deb892e560624bb0cd00098dc66
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-10 12:11:04 -0700

    asyn io

--- node/learnyounode/io_async.js
+++ node/learnyounode/io_async.js
@@ -1,5 +1,9 @@
 var fs = require('fs');
 
-let file = fs.readFile(process.argv[2]).toString().split("\n");
+let file = fs.readFile(process.argv[2], function(a) {
+  console.log(file)
+})
 
-console.log(file.length - 1);
+
+
+console.log(file.length);
