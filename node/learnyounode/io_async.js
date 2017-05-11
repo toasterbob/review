d739c7fc8fb26a85950ac3094e9bcdf953b3ade8
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-10 21:36:21 -0700

    asyn io

--- node/learnyounode/io_async.js
+++ node/learnyounode/io_async.js
@@ -1,9 +1,23 @@
 var fs = require('fs');
 
-let file = fs.readFile(process.argv[2], function(a) {
-  console.log(file)
-})
+
+let file = fs.readFile( process.argv[2], (err, data) => {
+  if (err) throw err;
+  data = data.toString().split("\n");
+  console.log(data.length - 1);
+});
 
 
 
-console.log(file.length);
+
+// var fs = require('fs')
+//     var file = process.argv[2]
+//
+//     fs.readFile(file, function (err, contents) {
+//       if (err) {
+//         return console.log(err)
+//       }
+//       // fs.readFile(file, 'utf8', callback) can also be used
+//       var lines = contents.toString().split('\n').length - 1
+//       console.log(lines)
+//     })
