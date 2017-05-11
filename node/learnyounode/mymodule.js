1f22b6bdf45d6c8c4de7ef3e0aceb12df37e3719
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-11 15:06:17 -0700

    modular

--- node/learnyounode/mymodule.js
+++ node/learnyounode/mymodule.js
@@ -4,9 +4,9 @@ var path = require("path");
 var file = process.argv[2];
 var ext = process.argv[3];
 
-module.exports = function (file, ext){
-  console.log(file);
-  console.log(ext); 
+module.exports = function (files, exts){
+  console.log(files);
+  console.log(exts); 
 };
 
 // module.exports = fs.readdir(file, (err, list) => {
