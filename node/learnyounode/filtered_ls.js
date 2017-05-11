460a04e85733da648e1dcf77d34ab651372c7d5f
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-10 22:24:11 -0700

    filtered ls

--- node/learnyounode/filtered_ls.js
+++ node/learnyounode/filtered_ls.js
@@ -1,8 +1,15 @@
 var fs = require('fs');
 var file = process.argv[2];
+var ext = process.argv[3];
 
 fs.readdir(file, (err, list) => {
   if (err) throw err;
   list = list.toString();
   console.log(list);
 });
+
+// fs.readFile( ext, (err, data) => {
+//   if (err) throw err;
+//   data = data.toString();
+//   console.log(data);
+// });
