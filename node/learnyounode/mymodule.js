85a0982a44d415a0235ce51ffef61c3d6642aaaf
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-11 16:28:57 -0700

    modular

--- node/learnyounode/mymodule.js
+++ node/learnyounode/mymodule.js
@@ -5,7 +5,19 @@ var file = process.argv[2];
 var ext = process.argv[3];
 
 module.exports = function (files, exts, callback){
-  
+  fs.readdir(file, (err, list) => {
+    if (err) throw err;
+    // list = list.toString().split(",");
+
+    list.forEach(filename => {
+      // console.log(`${filename} yy`);
+      // console.log(`.${ext}`)
+      if(path.extname(filename) === `.${ext}`){
+        return callback(null, filename);
+      }
+    });
+
+  });
 };
 
 // module.exports = fs.readdir(file, (err, list) => {
