c300b52cddc044891d3ce2a3a5199852c3cef803
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-10 22:37:32 -0700

    filtered ls

--- node/learnyounode/filtered_ls.js
+++ node/learnyounode/filtered_ls.js
@@ -17,17 +17,17 @@ fs.readdir(file, (err, list) => {
 
 });
 
-// var fs = require('fs')
-//     var path = require('path')
-//
-//     var folder = process.argv[2]
-//     var ext = '.' + process.argv[3]
-//
-//     fs.readdir(folder, function (err, files) {
-//       if (err) return console.error(err)
-//       files.forEach(function (file) {
-//         if (path.extname(file) === ext) {
-//           console.log(file)
-//         }
-//       })
-//     })
+var fs = require('fs')
+    var path = require('path')
+
+    var folder = process.argv[2]
+    var ext = '.' + process.argv[3]
+
+    fs.readdir(folder, function (err, files) {
+      if (err) return console.error(err)
+      files.forEach(function (file) {
+        if (path.extname(file) === ext) {
+          console.log(file)
+        }
+      })
+    })
