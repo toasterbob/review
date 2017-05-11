6cef2b15798173536ebb4cd201228f7f57509a47
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-11 13:28:44 -0700

    var vs let

--- challenges/js/random/var_vs_let.js
+++ challenges/js/random/var_vs_let.js
@@ -1,14 +1,22 @@
 
-let arr1 = [];
-let arr2 = [];
 
-for(var i = 0; i < 3; i++){
-  arr1.push(i);
+
+function foo1(){
+  let arr1 = [];
+  for(var i = 0; i < 3; i++){
+    setTimeout(() => {console.log(i)}, 0);
+  }
+  return arr1;
 }
 
-for(let j = 0; j < 3; j++){
-  arr2.push(j);
+function foo2(){
+  let arr2 = [];
+  for(let j = 0; j < 3; j++){
+    setTimeout(() => console.log(j), 0);
+  }
+  return arr2;
 }
 
-console.log(arr1);
-console.log(arr2);
+
+console.log(foo1());
+console.log(foo2());
