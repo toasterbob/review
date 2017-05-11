83b1e0dc0510b094d797b298548c96e8008f0b7a
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-11 16:11:45 -0700

    modular

--- node/learnyounode/modular.js
+++ node/learnyounode/modular.js
@@ -6,4 +6,22 @@ var path = require("path");
 var file = process.argv[2];
 var ext = process.argv[3];
 
-mymodule(file, ext);
+function bar (callback) {
+       foo(function (err, data) {
+         if (err)
+           return callback(err) // early return
+
+         // ... no error, continue doing cool things with `data`
+
+         // all went well, call callback with `null` for the error argument
+
+         callback(null, data)
+       })
+     }
+
+mymodule(file, ext, (err, data) => {
+    if(err) throw err;
+    data.forEach(line => {
+      console.log(line);
+    })
+});
