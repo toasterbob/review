a6668a7f9a7ea997741b19301036b15cd48440d3
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-10 22:50:30 -0700

    make it modular

---
+++ node/learnyounode/mymodule.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var path = require("path");
+
+var file = process.argv[2];
+var ext = process.argv[3];
+
+module.exports = fs.readdir(file, (err, list) => {
+  if (err) throw err;
+  // list = list.toString().split(",");
+  list.forEach(filename => {
+    // console.log(`${filename} yy`);
+    // console.log(`.${ext}`)
+    if(path.extname(filename) === `.${ext}`){
+      console.log(`${filename}`);
+    }
+  });
+
+});
