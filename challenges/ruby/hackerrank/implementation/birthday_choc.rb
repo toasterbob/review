6bbc3647ce975c86977988fd44dbcf90eccffd96
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-13 12:06:42 -0700

    bday choc

--- challenges/ruby/hackerrank/implementation/birthday_choc.rb
+++ challenges/ruby/hackerrank/implementation/birthday_choc.rb
@@ -1,19 +1,19 @@
 #!/bin/ruby
 
 def getWays(squares, d, m)
-    total = 0;
-    i = 0;
-    while i <= squares.length - m
-        total += 1 if squares[i, m].reduce(:+) == d
-        i += 1
+    total = 0
+
+    squares.each_cons(m) do |block|
+        total += 1 if block.reduce(:+) == d
     end
-    total
+
+    return total
 end
 
 n = gets.strip.to_i
 s = gets.strip
 s = s.split(' ').map(&:to_i)
-d, m = gets.strip.split(' ')
+d,m = gets.strip.split(' ')
 d = d.to_i
 m = m.to_i
 result = getWays(s, d, m)
