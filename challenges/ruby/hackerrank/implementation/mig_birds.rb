da2461539bd3739d838a2dfc985b04c3cf8ae64e
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-13 11:49:44 -0700

    migratory birds

--- challenges/ruby/hackerrank/implementation/mig_birds.rb
+++ challenges/ruby/hackerrank/implementation/mig_birds.rb
@@ -5,7 +5,7 @@ types = gets.strip
 types = types.split(' ').map(&:to_i)
 # your code goes here
 
-def most_common(arr)
+def most_common(arr) #sort array first
     arr.sort!
     most_common = 0
     most_common_count = 0
@@ -29,4 +29,24 @@ def most_common(arr)
     most_common
 end
 
-puts most_common(types)
+def most_common2(arr) #use hash
+    count = Hash.new(0)
+    best = 0
+    best_count = 0
+
+    arr.each do |type|
+        count[type] += 1
+    end
+
+    count.keys.each do |type|
+        if count[type] > best_count
+            best = type
+            best_count = count[type]
+        elsif count[type] == best_count
+            best = type if type < best
+        end
+    end
+    best
+end
+
+puts most_common2(types)
