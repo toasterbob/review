f73c8e5ef53b77530bb444d7bcc3a7a1d057160e
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-13 22:24:00 -0700

    sock merchant

--- challenges/ruby/hackerrank/implementation/sock_merchant.rb
+++ challenges/ruby/hackerrank/implementation/sock_merchant.rb
@@ -12,4 +12,15 @@ def socks(pile)
     pairs
 end
 
+def socks2(pile) #use a hash O(n)
+    pairs = 0
+    sock_hash = Hash.new(0)
+    pile.each do |color|
+       sock_hash[color] += 1
+       pairs += 1 if sock_hash[color] % 2 == 0
+    end
+    pairs
+end
+
+
 puts socks(c)
