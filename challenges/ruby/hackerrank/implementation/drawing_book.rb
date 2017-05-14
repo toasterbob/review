36025bc250b4ad9f4a574fbb674d30d4b38a651b
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-13 22:50:35 -0700

    drawing book

--- challenges/ruby/hackerrank/implementation/drawing_book.rb
+++ challenges/ruby/hackerrank/implementation/drawing_book.rb
@@ -1,7 +1,6 @@
 #!/bin/ruby
 
 def solve(n, p)
-    return 0 if p == 1 #edge case where page 1
     back = (n - p) / 2
     back += 1 if (n % 2 == 0 && n - 1 == p) #edge case where n is even and p is one less
     front = (p / 2)
