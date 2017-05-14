9dfcfc3c98495846fcddf4bf9104822163dff447
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-13 23:16:22 -0700

    counting valleys

---
+++ challenges/ruby/hackerrank/implementation/counting_valleys.rb
@@ -0,0 +1,20 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = gets.strip.to_i
+hike_str = gets.strip
+
+def trip(hike)
+    below_sea_level = false
+    altitude = 0
+    valleys = 0
+
+    hike.split("").each do |step|
+        valleys += 1 if altitude == 0 && step == "D"
+        altitude += 1 if step == "U"
+        altitude -= 1 if step == "D"
+    end
+
+    valleys
+end
+
+puts trip(hike_str)
