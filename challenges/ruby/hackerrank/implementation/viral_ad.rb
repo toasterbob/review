f0bf2b9314a170d20150d9ef8cbd73b6108c910b
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-16 17:29:55 -0700

    viral advertising

--- challenges/ruby/hackerrank/implementation/viral_ad.rb
+++ challenges/ruby/hackerrank/implementation/viral_ad.rb
@@ -1,1 +1,14 @@
- 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+def viral(n)
+   return 0 if n < 1
+   day = 5/2
+   total = 0
+   n.times do |i|
+       total += day
+       day = (day * 3) / 2
+   end
+   total
+end
+
+puts viral(gets.strip.to_i)
