bde577c8c6e572d27ed4bcc209c8e7a2f88fec67
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-18 14:32:08 -0700

    arr-hashes

--- challenges/ruby/hackerrank/ruby/arr_hashes.rb
+++ challenges/ruby/hackerrank/ruby/arr_hashes.rb
@@ -37,3 +37,23 @@ def delete_all(arr, val)
     # delete all the elements of the array where element = val
     arr.delete(val)
 end
+
+def select_arr(arr)
+  # select and return all odd numbers from the Array variable `arr`
+    arr.select{ |el| el % 2 == 1}
+end
+
+def reject_arr(arr)
+  # reject all elements which are divisible by 3
+    arr.reject{ |el| el % 3 == 0}
+end
+
+def delete_arr(arr)
+  # delete all negative elements
+    arr.delete_if{ |el| el < 0}
+end
+
+def keep_arr(arr)
+  # keep all non negative elements ( >= 0)
+    arr.keep_if{ |el| el >= 0}
+end
