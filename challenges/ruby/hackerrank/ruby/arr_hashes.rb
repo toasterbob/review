9bd82de1a6140114fcf94c3757e0f3e9550821b5
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-18 14:36:04 -0700

    arr-hashes

--- challenges/ruby/hackerrank/ruby/arr_hashes.rb
+++ challenges/ruby/hackerrank/ruby/arr_hashes.rb
@@ -57,3 +57,15 @@ def keep_arr(arr)
   # keep all non negative elements ( >= 0)
     arr.keep_if{ |el| el >= 0}
 end
+
+####################################################
+#Hash
+
+# Initialize 3 variables here as explained in the problem statement
+empty_hash = Hash.new
+default_hash = Hash.new(1)
+
+#default_hash = Hash.new
+#default_hash.default = 1
+
+hackerrank = {"simmy" => 100, "vivmbbs" => 200}
