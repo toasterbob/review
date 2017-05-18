3f2a7b5a536082969a48266275ed82a564137b08
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-18 14:26:04 -0700

    arr-hashes

---
+++ challenges/ruby/hackerrank/ruby/arr_hashes.rb
@@ -0,0 +1,39 @@
+def end_arr_add(arr, element)
+    # Add `element` to the end of the Array variable `arr` and return `arr`
+    arr.push(element)
+end
+
+def begin_arr_add(arr, element)
+    # Add `element` to the beginning of the Array variable `arr` and return `arr`
+    arr.unshift(element)
+end
+
+def index_arr_add(arr, index, element)
+    # Add `element` at position `index` to the Array variable `arr` and return `arr`
+    arr.insert(index, element)
+end
+
+def index_arr_multiple_add(arr, index)
+    # add any two elements to the arr at the index
+    arr.insert(index, "mark", "noizumi")
+end
+
+def end_arr_delete(arr)
+    # delete the element from the end of the array and return the deleted element
+    arr.pop
+end
+
+def start_arr_delete(arr)
+    # delete the element at the beginning of the array and return the deleted element
+    arr.shift
+end
+
+def delete_at_arr(arr, index)
+    # delete the element at the position #index
+    arr.delete_at(index)
+end
+
+def delete_all(arr, val)
+    # delete all the elements of the array where element = val
+    arr.delete(val)
+end
