d47b77f67fd3eed194f788e23faacbe92e7fc614
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-19 23:42:28 -0700

    lazy evaluation

--- challenges/ruby/hackerrank/ruby/lazy_eval.rb
+++ challenges/ruby/hackerrank/ruby/lazy_eval.rb
@@ -6,3 +6,24 @@ end
 puts power_array.(2 , 4)    #[1, 4, 9, 16]
 puts power_array.(2 , 10)   #[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 puts power_array.(3, 5)     #[1, 8, 27, 64, 125]
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+$pal_p = [2, 3, 5, 7, 11]
+$count = 12
+
+n = gets.strip.to_i
+require 'prime'
+
+def pal_primes(n)
+    while Primes
+    i = $count
+    while $pal_p.length < n
+      $pal_p << i if Prime.prime?(i) && i.to_s == i.to_s.reverse
+      i += 1
+    end
+    $pal_p
+
+end
+
+p pal_primes(n)
