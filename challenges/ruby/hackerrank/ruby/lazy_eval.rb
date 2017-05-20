3a4a263a8a38b8ebea8172afb12b603924a743db
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-19 23:04:10 -0700

    lazy evaluation

--- challenges/ruby/hackerrank/ruby/lazy_eval.rb
+++ challenges/ruby/hackerrank/ruby/lazy_eval.rb
@@ -1,6 +1,7 @@
 power_array = -> (power, array_size) do
     1.upto(Float::INFINITY).lazy.map { |x| x**power }.first(array_size)
 end
+#To avoid timeouts and memory allocation exceptions, we use lazy
 
 puts power_array.(2 , 4)    #[1, 4, 9, 16]
 puts power_array.(2 , 10)   #[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
