6ee6d4d9e5cd18192f602d568029f5bbb3448c21
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-20 23:07:06 -0700

    methods

--- challenges/ruby/hackerrank/ruby/temp_conversion.rb
+++ challenges/ruby/hackerrank/ruby/temp_conversion.rb
@@ -1,29 +1,3 @@
-def prime?(n)
-    return false if n < 2
-    return true if n == 2
-    2.upto(n - 1) do |i|
-        return false if n % i == 0
-    end
-    true
-end
-
-# Your code here
-def take(arr, n = 1)
-   arr[n..-1]
-end
-
-def take(arr, n = 1)
-    arr.drop(n)
-end
-
-##########################
-def foo(str: "foo", num: 424242, **options)
-  [str, num, options]
-end
-
-foo #=> ['foo', 424242, {}]
-foo(check: true) # => ['foo', 424242, {check: true}]
-#################################################
 def convert_temp(temp, input_scale: "celsius", output_scale: "celsius")
 
     if input_scale == "celsius"
@@ -39,4 +13,3 @@ def convert_temp(temp, input_scale: "celsius", output_scale: "celsius")
 
     temp
 end
-#############################################################
