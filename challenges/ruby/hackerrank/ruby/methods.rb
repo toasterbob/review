f7aead546997f8fe90866644c2279ef57a29bfb4
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-20 23:42:17 -0700

    methods

--- challenges/ruby/hackerrank/ruby/methods.rb
+++ challenges/ruby/hackerrank/ruby/methods.rb
@@ -89,3 +89,16 @@ factorial do
     puts 1.upto(n).inject(&:*)
 end
 #***********************************************
+
+def square_of_sum (my_array, proc_square, proc_sum)
+    sum = proc_sum.call(my_array)
+    proc_square.call(sum)
+end
+
+proc_square_number = proc {|num| num * num}
+proc_sum_array = proc { |arr| arr.reduce(&:+)}
+my_array = gets.split().map(&:to_i)
+
+puts square_of_sum(my_array, proc_square_number, proc_sum_array)
+
+#################################################################
