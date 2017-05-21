3b020e73b986f38e2f62718cdc90cea320b0b735
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-21 09:58:11 -0700

    methods

--- challenges/ruby/hackerrank/ruby/methods.rb
+++ challenges/ruby/hackerrank/ruby/methods.rb
@@ -102,3 +102,28 @@ my_array = gets.split().map(&:to_i)
 puts square_of_sum(my_array, proc_square_number, proc_sum_array)
 
 #################################################################
+# Write a lambda which takes an integer and square it
+square      = -> (n) { n *n }
+
+# Write a lambda which takes an integer and increment it by 1
+plus_one    = -> (n) { n += 1 }
+
+# Write a lambda which takes an integer and multiply it by 2
+into_2      = -> (n) { n * 2 }
+
+# Write a lambda which takes two integers and adds them
+adder       = -> (x, y) { x + y }
+
+# Write a lambda which takes a hash and returns an array of hash values
+values_only = -> (hash) { hash.values }
+
+
+input_number_1 = gets.to_i
+input_number_2 = gets.to_i
+input_hash = eval(gets)
+
+a = square.(input_number_1); b = plus_one.(input_number_2);c = into_2.(input_number_1);
+d = adder.(input_number_1, input_number_2);e = values_only.(input_hash)
+
+p a; p b; p c; p d; p e
+#############################################################################
