c217f6dcd6efc13f8d6ff2e3406b4d2ccfdcb0d7
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-21 23:38:27 -0700

    ice cream

--- challenges/ruby/hackerrank/graph_theory/even_tree.rb
+++ challenges/ruby/hackerrank/graph_theory/even_tree.rb
@@ -30,6 +30,8 @@ puts tree.count(1) - 1
 
 
 
+
+
 ####################################################
 (vertices, edges) = gets.split.map{|s| s.to_i}
 tree = Array.new(vertices + 1)
@@ -52,12 +54,12 @@ result = 0
 puts tree
 puts map
 (tree.size - 1).downto(1) do |node_index|
-  if(map[node_index] % 2 == 0)
+  if(hmap[node_index] % 2 == 0)
     hasEven = false
     (tree.size - 1).downto(1) do |i|
       if(tree[i] == node_index)
         node = i
-        if(map[node] % 2 == 0)
+        if(hmap[node] % 2 == 0)
           hasEven = true
         end
       end
@@ -67,8 +69,8 @@ puts map
       parent = tree[node_index]
       tree[node_index] = 0
       puts parent
-      count2 = map[parent]
-      map[parent] = count2 - 1
+      count2 = hmap[parent]
+      hmap[parent] = count2 - 1
     end
   end
 end
