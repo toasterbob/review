86f992a48e87c9f7a68362bd78dbdf3c0ee0e008
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-22 23:15:49 -0700

    missing numbers

--- challenges/ruby/hackerrank/search/missing_nums.rb
+++ challenges/ruby/hackerrank/search/missing_nums.rb
@@ -13,3 +13,24 @@ list_b.sort.uniq.each do |num|
         print " "
     end
 end
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+#using a hash 
+gets
+list_a = gets.strip.split(" ").map(&:to_i)
+gets
+list_b = gets.strip.split(" ").map(&:to_i)
+
+hash_a = Hash.new(0)
+list_a.each{|num| hash_a[num] += 1}
+hash_b = Hash.new(0)
+list_b.each{|num| hash_b[num] += 1}
+
+
+list_b.sort.uniq.each do |num|
+
+    if (hash_b[num] - hash_a[num]) != 0
+        print num
+        print " "
+    end
+end
