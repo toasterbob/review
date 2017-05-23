347a4ec60d8f54125874235ee0a2e86abbe72795
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-22 23:22:05 -0700

    missing numbers with hash

--- challenges/ruby/hackerrank/search/missing_nums.rb
+++ challenges/ruby/hackerrank/search/missing_nums.rb
@@ -16,21 +16,23 @@ end
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 #using a hash
+def missing(list_a, list_b)
+    hash_a = Hash.new(0) # use hash for quicker counts
+    list_a.each{|num| hash_a[num] += 1}
+    hash_b = Hash.new(0)
+    list_b.each{|num| hash_b[num] += 1}
+
+
+    hash_b.keys.sort.each do |num|
+        if (hash_b[num] - hash_a[num]) != 0
+            print num
+            print " "
+        end
+    end
+end
+
 gets
 list_a = gets.strip.split(" ").map(&:to_i)
 gets
 list_b = gets.strip.split(" ").map(&:to_i)
-
-hash_a = Hash.new(0)
-list_a.each{|num| hash_a[num] += 1}
-hash_b = Hash.new(0)
-list_b.each{|num| hash_b[num] += 1}
-
-
-hash_b.keys.sort.each do |num|
-
-    if (hash_b[num] - hash_a[num]) != 0
-        print num
-        print " "
-    end
-end 
+missing(list_a, list_b)
