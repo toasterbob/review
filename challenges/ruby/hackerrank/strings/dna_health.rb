7f81b451ec194aba7c53bb992871a8ea1e27d637
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-24 22:00:28 -0700

    determining dna health

---
+++ challenges/ruby/hackerrank/strings/dna_health.rb
@@ -0,0 +1,30 @@
+n = gets.strip.to_i
+genes = gets.strip
+genes = genes.split(' ')
+health = gets.strip
+health = health.split(' ').map(&:to_i)
+s = gets.strip.to_i
+health_arr = []
+
+for a0 in (0..s-1)
+    first,last,d = gets.strip.split(' ')
+    first = first.to_i
+    last = last.to_i
+    count = 0
+
+    first.upto(last) do |i|
+        check = genes[i]
+
+        c_length = check.length
+
+        j = 0
+        while j <= (d.length - c_length)
+
+            count += health[i] if d.slice(j, c_length) == check
+            j += 1
+        end
+    end
+    health_arr << count
+end
+
+puts "#{health_arr.min} #{health_arr.max}"
