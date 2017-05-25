d304cf2372c5afc6b4e53407cc00d76e53e6e8e2
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-24 17:49:15 -0700

    pairs

--- challenges/ruby/hackerrank/search/pairs.rb
+++ challenges/ruby/hackerrank/search/pairs.rb
@@ -2,11 +2,14 @@ def  pairs( a,k)
     #a is an array containing numbers and k is the difference.
     count = 0
     h = Hash.new(false)
-    a.each do |num|
+
+    a.each do |num|  #O(n)
         h[num] = true
     end
+
     a.each do |num|
-        count += 1 if h[num - k]
+        count += 1 if h[num - k] #lookup O(1)
     end
+
     count
 end
