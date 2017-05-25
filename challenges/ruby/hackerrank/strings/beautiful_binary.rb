2a3836e02de85e386afdaf414e046a266c1e812f
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-24 17:06:52 -0700

    beautiful binary

---
+++ challenges/ruby/hackerrank/strings/beautiful_binary.rb
@@ -0,0 +1,19 @@
+#!/bin/ruby
+
+def minSteps(n, b)
+    i = 0
+    count = 0
+    while i < n
+        if b[i] == "0" && b[i + 1] == "1" && b [i + 2] == "0"
+            count += 1
+            i += 2
+        end
+        i += 1
+    end
+    count
+end
+
+n = gets.strip.to_i
+b = gets.strip
+result = minSteps(n, b)
+puts result;
