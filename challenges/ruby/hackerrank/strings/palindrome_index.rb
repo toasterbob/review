750f34172cc6b271cbebc0265f6128ead6a83a32
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-25 16:29:26 -0700

    palindrome index

--- challenges/ruby/hackerrank/strings/palindrome_index.rb
+++ challenges/ruby/hackerrank/strings/palindrome_index.rb
@@ -10,8 +10,8 @@ def palindrome(str)
         j -= 1
     end
 
-    return i if str[i + 1] == str[j]
-    return j if str[i] == str[j-1]
+    return i if str[i + 1] == str[j] && str[i + 2] == str[j - 1]
+    return j if str[i] == str[j - 1] && str[i + 1] == str[j - 2]
 end
 
 n.times do
