d3f20cb61430eed2e0cfb2fd72388093b1c3411d
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-25 19:57:03 -0700

    hoisting

--- rithm/js/hoisting.js
+++ rithm/js/hoisting.js
@@ -14,7 +14,7 @@ sayHi();
 
 var firstFunction = function(){
   return "HI!";
-}
+};
 firstFunction();
 
 
@@ -32,3 +32,25 @@ function sayHello(){
  };
 // sayHello hoists but it is undefined so it can't be invoked
 // gives error
+
+// es6 const and let
+// const does not mean immutability - no redeclaration - but variable can change
+const numbers = [1, 2, 3, 4];
+numbers.push(100);
+numbers;
+
+numbers.length = 0; // [] - but don't do this hack, bad programming
+
+// let gives us block scope {}
+
+for(var i = 0; i < 5; i++){
+  (function(j){
+    setTimeout(function(){
+      console.log(j)
+    }, 0)
+  })(i)  // passing the variable at the time into it
+}
+
+let instructor = "elie";
+instructor = "bob";
+let instructor = "elie"; //error
