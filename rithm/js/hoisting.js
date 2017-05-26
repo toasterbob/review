e845b042f326e1510df56b322097df53172036e4
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-25 19:36:46 -0700

    hoisting

--- rithm/js/hoisting.js
+++ rithm/js/hoisting.js
@@ -5,8 +5,30 @@ var thing = "nice";
 
 function sayHi(){
   return elie;
-  var elie = "Instructor"; // hoists elie to the top but it's undefined 
+  var elie = "Instructor"; // hoists elie to the top but it's undefined
 
 }
 
 sayHi();
+
+
+var firstFunction = function(){
+  return "HI!";
+}
+firstFunction();
+
+
+sayHello(); // returns "HELLO"
+
+function sayHello(){
+  return "HELLO!";
+}
+ // function declarations always hoist
+
+ sayHello2(); //sayHello2 is not a function
+
+ var sayHello2 = function(){
+   return "Hello!";
+ };
+// sayHello hoists but it is undefined so it can't be invoked
+// gives error
