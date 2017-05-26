cff53736213a7f03b38881f0cd16dd5c5f63bb26
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-26 10:39:04 -0700

    es6 classes

--- rithm/js/es6.js
+++ rithm/js/es6.js
@@ -13,4 +13,18 @@ function Student(first, last){
 
 }
 
-new Student("Tim", "Garcia"); // creates an implicit return
+new Student("Tim", "Garcia"); // creates {}, assign this = {},
+// return this - creates an implicit return, creates link between
+//that object and the protype property of the function just run
+
+Student.prototype.sayHi = function(){
+  return "hi!";
+};
+
+class Person {
+  constructor(first, last){
+    this.first = first;
+    this.last = last;
+  }
+
+}
