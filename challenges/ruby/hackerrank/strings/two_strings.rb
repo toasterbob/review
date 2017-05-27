3f305fbed4f3e5e9cfb76b59d4a50a4b4b21b2f6
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-27 00:16:46 -0700

    two strings

---
+++ challenges/ruby/hackerrank/strings/two_strings.rb
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def common_sub(str1, str2)
+    str1.split("").each do |char|
+        return "YES" if str2.include?(char)
+    end
+    "NO"
+end
+
+n = gets.strip.to_i
+
+n.times do
+    str1 = gets.strip
+    str2 = gets.strip
+    puts common_sub(str1, str2)
+end
