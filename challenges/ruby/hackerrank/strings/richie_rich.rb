00f31a2ed13a323c7666ab01aba6af2b8cce7a46
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-28 00:06:44 -0700

    richie rich

--- challenges/ruby/hackerrank/strings/richie_rich.rb
+++ challenges/ruby/hackerrank/strings/richie_rich.rb
@@ -1,5 +1,19 @@
 #!/bin/ruby
+def checker(num)
+    differences = 0
+    i = 0
+    j = num.length - 1
+    while i < j
+        differences += 1 if num[i] != num[j]
+        i += 1
+        j -= 1
+    end
+    differences
+end
+
 def biggest_palindrome(k, num)
+    dif = checker(num)
+    return -1 if dif > k
     answers = []
     k.times do
         i = 0
@@ -17,4 +31,5 @@ end
 n,k = gets.strip.split(' ')
 n = n.to_i
 k = k.to_i
-number = gets.strip 
+number = gets.strip
+puts biggest_palindrome(k, number)
