579074134e3c9dcc935862ce5f6f7d8351f2bad2
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-27 23:18:13 -0700

    sherlock and the valid string

--- challenges/ruby/hackerrank/strings/sherlock_valid_string.rb
+++ challenges/ruby/hackerrank/strings/sherlock_valid_string.rb
@@ -10,12 +10,12 @@ def isValid(s)
     return "YES" if arr.length < 2
     return "NO" if arr.length > 2 #no if there is more than 2 unique values
     if arr.length == 2
-        min_count = arr.min
-        return "YES" if min_count == 1 && hash.values.count(1) == 1 #single value of 1 in array
+        min, max = arr.sort
+        return "YES" if min == 1 && hash.values.count(1) == 1 #single value of 1 in array
         return "NO" if hash.values.count(arr[0]) > 1 && hash.values.count(arr[1]) > 1
-        keys = hash.keys
-        min, max = keys.sort
-
+        low_count = hash.values.count(arr[0]) == 1 ? arr[0] : arr[1]
+        return "YES" if min = max - 1 && low_count == max
+        return "NO"
     end
 end
 
