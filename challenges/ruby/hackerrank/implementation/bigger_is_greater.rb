75e1a12e6d48abf4f4f8d727df782c54882e1695
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-31 12:24:32 -0700

    bigger is greater

--- challenges/ruby/hackerrank/implementation/bigger_is_greater.rb
+++ challenges/ruby/hackerrank/implementation/bigger_is_greater.rb
@@ -1,4 +1,4 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+
 def no_answer(str)
     arr = str[1..-1].split("")
     valid = false
@@ -16,15 +16,12 @@ end
 def greater(str)
     return "no answer" unless no_answer(str)
     i = str.length - 2
-    #p [i]
-    while i >= 0 #go from the back and switch bigger with smaller
 
+    while i >= 0 #go from the back and switch bigger with smaller
         char1 = str[i]
-        #p [char1]
         remaining = str[i + 1..-1]
-        #p remaining
+
         remaining.split("").sort.uniq.each do |char2|
-           #p [char1, char2]
            if  char2 > char1
                j = -(remaining.reverse.index(char2) + 1) #find the instance of that char closest to the end of arr
                str[j], str[i] = str[i], str[j] #swap
@@ -41,5 +38,4 @@ n = gets.strip.to_i
 n.times do
     str = gets.strip
     puts greater(str)
-
 end
