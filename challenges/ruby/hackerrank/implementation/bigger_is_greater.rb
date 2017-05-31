de2d21d4e088a7952c0076d4c1c103dc5b34b71b
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-30 22:36:32 -0700

    bigger is greater

---
+++ challenges/ruby/hackerrank/implementation/bigger_is_greater.rb
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def greater(str)
+
+end
+
+
+n = gets.strip.to_i
+
+n.times do
+    str = gets.strip
+    puts greater(str)
+
+end
