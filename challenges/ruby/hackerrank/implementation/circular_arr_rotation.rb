4330ac224ebbfd90b0ba2453bc3936fb948d3232
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-31 14:19:50 -0700

    circular array rotation

--- challenges/ruby/hackerrank/implementation/circular_arr_rotation.rb
+++ challenges/ruby/hackerrank/implementation/circular_arr_rotation.rb
@@ -14,3 +14,19 @@ for a0 in (0..q-1)
     m = gets.strip.to_i
     puts arr[m]
 end
+
+#################
+#negative rotation
+def rotate(n, arr)
+  if n == 0
+    arr
+  elsif n < 0
+    n = (-n % arr.length)
+    return arr if n == 0
+    arr.drop(n) + arr.take(n)
+  elsif n > 0
+    n = n % arr.length
+    return arr if n == 0
+    arr.drop(arr.length - n) + arr.take(arr.length - n)
+  end
+end
