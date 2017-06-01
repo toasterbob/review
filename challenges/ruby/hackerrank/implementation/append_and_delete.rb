9a66d4fd4ecaf3f4b57b88751b93bd46445ed3c0
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-31 17:32:47 -0700

    append and delete

--- challenges/ruby/hackerrank/implementation/append_and_delete.rb
+++ challenges/ruby/hackerrank/implementation/append_and_delete.rb
@@ -5,13 +5,22 @@ t = gets.strip
 k = gets.strip.to_i
 
 i = 0
+flag = false
+
 while i < t.length
     if s[i] != t[i]
-        add = s.length - i - 1
-        left = t.length - i - 1
-        puts (k - add + left) % 2 == 0 ?  "Yes" : "No"
+        add = s.length - i
+        left = t.length - i
+        puts (k - (add + left)) >= 0 &&  (k - (add + left)) % 2 == 0 ?  "Yes" : "No"
+        flag = true
         i = t.length
     end
     i += 1
 end
-puts "Yes" if s == t
+
+if s == t
+    puts "Yes" if s == t
+    flag = true
+end
+
+puts (k - (s.length - t.length)) % 2 == 0 ? "Yes" : "No" unless flag 
