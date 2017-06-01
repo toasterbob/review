04e3ea0cb69f6b8269b6115eb415db9a5b39c983
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-01 14:34:34 -0700

    sherlock and the squares

--- challenges/ruby/hackerrank/implementation/sherlock_squares.rb
+++ challenges/ruby/hackerrank/implementation/sherlock_squares.rb
@@ -1,4 +1,14 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+# not as good of time complexity
+squares = (1..31622).map { |n| n ** 2 }
+
+gets.to_i.times do
+  a, b = gets.split.map(&:to_i)
+  puts squares.select { |n| n.between?(a, b) }.size
+end
+##################################################
+
+#O(1) time 
 def squares(x, y)
     count = 0
     x, y = Math.sqrt(x), Math.sqrt(y)
