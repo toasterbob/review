7555760d20982d750f431b93deb580c20758035a
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-31 22:38:55 -0700

    equalize

--- challenges/ruby/hackerrank/implementation/equalize_arr.rb
+++ challenges/ruby/hackerrank/implementation/equalize_arr.rb
@@ -8,3 +8,18 @@ arr.uniq.each do |el|
 end
 
 puts result.reduce(:+) - result.max
+
+### refactor hash
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+gets
+arr = gets.strip.split(" ")
+
+result = Hash.new(0)
+arr.each do |el|
+    result[el] += 1
+end
+
+result = result.values
+
+puts result.reduce(:+) - result.max
