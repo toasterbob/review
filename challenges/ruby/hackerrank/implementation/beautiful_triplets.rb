dd42fd8e0a4bb8e6e7ed44762d6cf7756315c26c
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-01 13:50:35 -0700

    beautiful triplets

--- challenges/ruby/hackerrank/implementation/beautiful_triplets.rb
+++ challenges/ruby/hackerrank/implementation/beautiful_triplets.rb
@@ -1,16 +1,11 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
 n, d = gets.strip.split(" ").map(&:to_i)
-arr = gets.strip.split(" ").map(&:to_i)
-
 nums = Hash.new(false)
+arr = gets.strip.split(" ").map { |el| nums[el.to_i] = true}
 count = 0
 
-arr.each do |el|
-    nums[el] = true
-end
-
-arr.each do |el|
+nums.keys.each do |el|
     count += 1 if nums[el + d] && nums[el + d + d]
 end
 
