be341a817d3217e4e784291d4bf017f53e031c5d
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-01 23:30:31 -0700

    library fine with case statement

--- challenges/ruby/hackerrank/implementation/library_fine.rb
+++ challenges/ruby/hackerrank/implementation/library_fine.rb
@@ -22,3 +22,32 @@ elsif d1 > d2
 else
     puts 0
 end
+
+
+#refactored with case statement
+
+#!/bin/ruby
+
+d1,m1,y1 = gets.strip.split(' ')
+d1 = d1.to_i
+m1 = m1.to_i
+y1 = y1.to_i
+d2,m2,y2 = gets.strip.split(' ')
+d2 = d2.to_i
+m2 = m2.to_i
+y2 = y2.to_i
+
+case
+    when y1 > y2
+        puts 10000
+    when y1 < y2
+        puts 0
+    when m1 > m2
+        puts (m1 - m2) * 500
+    when m1 < m2
+        puts 0
+    when d1 > d2
+        puts (d1 - d2) * 15
+    else
+        puts 0
+end
