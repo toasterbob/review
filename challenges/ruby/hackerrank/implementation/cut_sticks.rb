c84b88a08d75027fc25abfbbd3bca89aa80a4463
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-01 23:44:20 -0700

    cut the sticks

---
+++ challenges/ruby/hackerrank/implementation/cut_sticks.rb
@@ -0,0 +1,18 @@
+#!/bin/ruby
+
+def cuts(arr)
+    result = [arr.length]
+
+    while arr.length > 1
+        cut = arr.min
+        arr = arr.map { |el| el - cut }.select { |el| el > 0}
+        result << arr.length
+    end
+    result
+end
+
+n = gets.strip.to_i
+arr = gets.strip
+arr = arr.split(' ').map(&:to_i)
+
+puts cuts(arr)
