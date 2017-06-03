d10d4f5f73424dd446fd8b04cc97eff64fbb8718
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-02 21:40:49 -0700

    queen attack

--- challenges/ruby/hackerrank/implementation/queen_attack2.rb
+++ challenges/ruby/hackerrank/implementation/queen_attack2.rb
@@ -1,8 +1,37 @@
 #!/bin/ruby
+def go(direction, pos, obs, board)
+    count = 0
+    pos[0] += direction[0]
+    pos[1] += direction[1]
+
+    condition1 = pos[0] > board || pos[1] > board
+    condition2 = pos[0] < 0 || pos[1] < 0
+
+    while !(obs.include?(pos) || pos[0] > board || pos[1] > board || pos[0] < 0 || pos[1] < 0)
+        p pos
+        count += 1
+        pos[0] += direction[0]
+        pos[1] += direction[1]
+
+    end
+
+    count
+end
+
+def queen(board, pos, obs)
+    count = 0
+    directions = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, -1], [-1, 1], [1, -1]]
+
+    directions.each do |direction|
+        count += go(direction, pos, obs, board)
+    end
+
+    count
+end
 
 n,k = gets.strip.split(' ').map(&:to_i)
 
-rowQ, colQ = gets.strip.split(' ').map(&:to_i)
+q_pos = gets.strip.split(' ').map(&:to_i)
 
 obstacles = []
 for a0 in (0..k-1)
@@ -10,4 +39,4 @@ for a0 in (0..k-1)
     obstacles << [rObstacle, cObstacle]
 end
 
-p obstacles
+p queen(n, q_pos, obstacles)
