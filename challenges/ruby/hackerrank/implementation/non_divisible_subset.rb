4be461794c54929e8cc9197b6e75447355241f7d
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-02 19:47:52 -0700

    non-divisible subset

--- challenges/ruby/hackerrank/implementation/non_divisible_subset.rb
+++ challenges/ruby/hackerrank/implementation/non_divisible_subset.rb
@@ -3,16 +3,22 @@ n, k = gets.strip.split(" ").map(&:to_i)
 arr = gets.strip.split(" ").map(&:to_i)
 
 hash = Hash.new(0)
-arr.delete(k)
+
 arr.map { |el| hash[el % k] +=1 }
+hash[0] = 1 if hash[0] > 0
 count = 0
 
 
 hash.keys.each do |i|
+    hash[i] = 1 if i + i == k && hash[i] > 0
     j = k - i
     hash[i] <= hash[j] ? count += hash[j] : count += hash[i]
     hash.delete(j)
     hash.delete(i)
 end
 
-puts count
+if k == 1
+    puts 1
+else
+    puts count == 0 ? 1 : count
+end
