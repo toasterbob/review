e6563b5df95af6be61d3e025db13f352d0c481c5
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-02 20:07:04 -0700

    non-divisible subset

--- challenges/ruby/hackerrank/implementation/non_divisible_subset.rb
+++ challenges/ruby/hackerrank/implementation/non_divisible_subset.rb
@@ -4,21 +4,16 @@ arr = gets.strip.split(" ").map(&:to_i)
 
 hash = Hash.new(0)
 
-arr.map { |el| hash[el % k] +=1 }
-hash[0] = 1 if hash[0] > 0
+arr.map { |el| hash[el % k] +=1 } # I modulus everything by k, this makes the numbers more managble
+hash[0] = 1 if hash[0] > 0 # we can only have one 0 (i.e. multiple of four) because any two could add together
 count = 0
 
-
 hash.keys.each do |i|
-    hash[i] = 1 if i + i == k && hash[i] > 0
-    j = k - i
-    hash[i] <= hash[j] ? count += hash[j] : count += hash[i]
-    hash.delete(j)
+    hash[i] = 1 if i + i == k && hash[i] > 0 # if any number + itself adds to k, we can only keep one
+    j = k - i # every i only has one set that can match to it
+    hash[i] <= hash[j] ? count += hash[j] : count += hash[i] # so we choose the bigger set
+    hash.delete(j) # i delete both so we don't compare it again
     hash.delete(i)
 end
 
-if k == 1
-    puts 1
-else
-    puts count == 0 ? 1 : count
-end
+puts count
