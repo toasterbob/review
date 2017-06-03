159d33b01a5551fcebe5a6bc57ab0a0e525c18fa
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-03 15:39:30 -0700

    time in words

--- challenges/ruby/hackerrank/implementation/time_in_words.rb
+++ challenges/ruby/hackerrank/implementation/time_in_words.rb
@@ -32,14 +32,13 @@ end
 h = gets.strip.to_i
 m = gets.strip.to_i
 
-hours = %w(zero one two three four five six seven eight nine ten eleven twelve)
-mins = %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty)
+words = %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty)
 1.upto(9) do |i|
-    mins << "twenty #{mins[i]}"
+    words << "twenty #{words[i]}"
 end
 
 h2 = h != 12 ? h + 1 : 1
-hour = m <= 30 ? hours[h] : hours[h2]
+hour = m <= 30 ? words[h] : words[h2]
 
 if m == 0
     puts "#{hour} o' clock"
@@ -48,8 +47,8 @@ elsif m % 15 == 0
     puts "half past #{hour}" if m == 30
     puts "quarter to #{hour}" if m == 45
 elsif m < 30
-    puts "#{mins[m]} minute#{m > 1 ? "s" : ""} past #{hour}"
+    puts "#{words[m]} minute#{m > 1 ? "s" : ""} past #{hour}"
 elsif m > 30
     m = 60 - m
-    puts "#{mins[m]} minute#{m > 1 ? "s" : ""} to #{hour}"
+    puts "#{words[m]} minute#{m > 1 ? "s" : ""} to #{hour}"
 end
