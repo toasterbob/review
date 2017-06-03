45b0f4fbaaf8fbb35aeef1000c1e2b7f71c6351d
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-03 12:57:55 -0700

    project euler 1

--- challenges/ruby/hackerrank/project_euler/one_mult_3_5.rb
+++ challenges/ruby/hackerrank/project_euler/one_mult_3_5.rb
@@ -12,3 +12,60 @@ for a0 in (0..t-1)
     n = gets.strip.to_i
     puts three_five(n)
 end
+
+##################################################################
+#!/bin/ruby
+def three_five(n)
+    result = []
+    i = 3
+    while i < n
+        result << i
+        i += [3 - (i % 3), 5 - (i % 5)].min
+    end
+    result.reduce(0, :+)
+end
+
+t = gets.strip.to_i
+for a0 in (0..t-1)
+    n = gets.strip.to_i
+    puts three_five(n)
+end
+####################################################
+#!/bin/ruby
+def three_five(n)
+    result = 0
+    i = 3
+    while i < n
+        result += i
+        i += [3 - (i % 3), 5 - (i % 5)].min
+    end
+    result
+end
+
+t = gets.strip.to_i
+for a0 in (0..t-1)
+    n = gets.strip.to_i
+    puts three_five(n)
+end
+#########################################################
+def three_five(n)
+    n -= 1
+    result = 0
+
+    threes = (n / 3)
+    result += (threes * (threes * 3 + 3))/2
+
+    fives = (n / 5)
+    result += (fives * (fives * 5 + 5))/2
+
+    fifteens = (n/15)
+    result -= (fifteens * (fifteens * 15 + 15))/2
+
+    result
+end
+
+t = gets.strip.to_i
+for a0 in (0..t-1)
+    n = gets.strip.to_i
+    puts three_five(n)
+end
