c08b2d121b5aa70aef4401be456ca8c0032fb411
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-03 18:31:32 -0700

    flatland space station

--- challenges/ruby/hackerrank/implementation/flatland_space.rb
+++ challenges/ruby/hackerrank/implementation/flatland_space.rb
@@ -1,14 +1,17 @@
 #!/bin/ruby
 
 n,m = gets.strip.split(' ').map(&:to_i)
-c = gets.strip.split(' ').map(&:to_i)
+c = gets.strip.split(' ').map(&:to_i).sort
 
 i = 0
 max = 0
 
 while i < c.length && n != m
     j = i + 1
-    if j < c.length
+    if i == 0
+        distance = [c[i], 100 - 1 - c[i]].max if m == 1
+        distance = [c[i], (c[j] - c[i])/2].max if m > 1
+    elsif j < c.length
         distance = (c[j] - c[i])/2
     else
         distance =  ((n -1) - c[i])
