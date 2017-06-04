93f141f934919ab30a03ee377d08d850fdf21d86
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-04 01:21:14 -0700

    cavity map

--- challenges/ruby/hackerrank/implementation/cavity_map.rb
+++ challenges/ruby/hackerrank/implementation/cavity_map.rb
@@ -9,10 +9,13 @@ end
 1.upto(n - 2) do |i|
     1.upto(grid[i].length - 2) do |j|
         compare = grid[i][j]
+
         up, down = grid[i - 1][j], grid[i + 1][j]
         left, right = grid[i][j - 1], grid[i][j + 1]
+
         cond1 = compare > up && compare > down
         cond2 = compare > left && compare > right
+
         grid[i][j] = "X" if  cond1 && cond2
     end
 end
