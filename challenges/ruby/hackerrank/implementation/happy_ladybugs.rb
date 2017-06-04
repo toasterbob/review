d1cc193173238be3ce66043f20bce5c81568ea4a
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-04 12:04:35 -0700

    Happy Ladybugs

--- challenges/ruby/hackerrank/implementation/happy_ladybugs.rb
+++ challenges/ruby/hackerrank/implementation/happy_ladybugs.rb
@@ -19,3 +19,26 @@ for a0 in (0..q-1)
     b = gets.strip
     puts happy(b)
 end
+
+#refactor
+
+#!/bin/ruby
+def happy(b)
+    happy = ""
+    sad = ""
+    b.split("").uniq.each { |el|  b.count(el) > 1 ? happy += (el * b.count(el)) : sad += el }
+    sad.delete!("_")
+
+    return "YES" if b.count("_") == 0 && happy == b
+
+    return "YES" if b.count("_") > 0 && sad.length == 0
+
+    "NO"
+end
+
+q = gets.strip.to_i
+for a0 in (0..q-1)
+    n = gets.strip.to_i
+    b = gets.strip
+    puts happy(b)
+end
