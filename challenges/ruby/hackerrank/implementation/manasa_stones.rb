dbb4135bf2229d60949febc36130f8cfd3bf9287
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-04 11:03:31 -0700

    Manasa and the Stones

--- challenges/ruby/hackerrank/implementation/manasa_stones.rb
+++ challenges/ruby/hackerrank/implementation/manasa_stones.rb
@@ -21,3 +21,26 @@ n.times do
     dif2 = gets.strip.to_i
     stones(length, dif1, dif2)
 end
+
+#refactor
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def stones(len, dif1, dif2)
+    result = [dif1 * (len - 1)]
+    difference = (dif2 - dif1).abs
+
+    (len - 1).times do
+        result << result[-1] + difference
+    end
+
+    puts result.join(" ")
+end
+
+n = gets.strip.to_i
+
+n.times do
+    length = gets.strip.to_i
+    dif1 = gets.strip.to_i
+    dif2 = gets.strip.to_i
+    stones(length, dif1, dif2)
+end
