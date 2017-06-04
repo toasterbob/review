0f202fc9f399d53e393e56f1fa4932d7f7fdd900
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-04 10:48:46 -0700

    Manasa and the Stones

---
+++ challenges/ruby/hackerrank/implementation/manasa_stones.rb
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def stones(len, dif1, dif2)
+    result = []
+    arr = [0]
+
+end
+
+n = gets.strip.to_i
+
+n.times do
+    length = gets.strip.to_i
+    dif1 = gets.strip.to_i
+    dif2 = gets.strip.to_i
+    stones(length, dif1, dif2)
+end
