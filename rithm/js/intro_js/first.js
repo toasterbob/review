456cb70573524e6cf0c58ce7d1eab611d5d43421
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-04 16:00:06 -0700

    first.js

--- rithm/js/intro_js/first.js
+++ rithm/js/intro_js/first.js
@@ -1,3 +1,5 @@
 alert("JS is cool"); //alert doesn't work in node
 //about:blank  - when using the console
-console.log("This is less obtrusive.");
+let cheese = prompt("Do you like cheese?");
+let result = confirm("You are dumb!"); // true or false return via cancel or okay
+console.log("This is less obtrusive. Cheese = ", cheese, result);
