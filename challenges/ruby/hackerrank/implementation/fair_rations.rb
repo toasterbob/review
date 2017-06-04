247cae1735bed9ce914368ee2568477147d2a8ae
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-03 21:36:22 -0700

    fair rations

--- challenges/ruby/hackerrank/implementation/fair_rations.rb
+++ challenges/ruby/hackerrank/implementation/fair_rations.rb
@@ -1,8 +1,18 @@
 #!/bin/ruby
 
-def bread(n, b)
-    return "NO" if b.length == 2 && (b[0] - b[1]).abs == 1
+def bread(n, arr)
+    return "NO" if n == 2 && (arr[0] - arr[1]).abs == 1
+    bread = 0
 
+    0.upto(n - 1) do |i|
+        j = i + 1
+        if arr[i] % 2 == 1 && i != (n - 1)
+            arr[i] += 1
+            arr[j] += 1
+            bread += 2
+        end
+    end
+    arr[-1] % 2 == 1 ? "NO" : bread
 end
 
 n = gets.strip.to_i
