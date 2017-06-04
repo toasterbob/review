751d0e59b566a3aa824ca3175c0be8fe94cdeda3
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-04 10:58:41 -0700

    Manasa and the Stones

--- challenges/ruby/hackerrank/implementation/manasa_stones.rb
+++ challenges/ruby/hackerrank/implementation/manasa_stones.rb
@@ -1,7 +1,15 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 def stones(len, dif1, dif2)
     result = []
-    arr = [0]
+
+    combos = []
+    (len - 1).times do
+        combos << dif1
+        combos << dif2
+    end
+
+    combos = combos.combination(len - 1).to_a.map { |arr| arr.reduce(:+) }.uniq
+    puts combos.sort.join(" ")
 
 end
 
