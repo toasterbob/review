4873dfae380f49c9bf336423340d3cf5c4ff36e4
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-05 16:48:46 +0530

    the DOM

---
+++ rithm/js/intermediate1/DOM.js
@@ -0,0 +1,33 @@
+//What is the DOM?
+
+// The Document Object Model (DOM) is a programming interface for HTML,
+// XML and SVG documents. It provides a structured representation of the document as a tree.
+
+// The DOM provides a representation of the document as a structured group
+// of nodes and objects, possessing various properties and methods. Nodes
+// can also have event handlers attached to them, and once an event is
+// triggered, the event handlers get executed. Essentially, it connects web
+// pages to scripts or programming languages.
+
+// To access the DOM, we make use of the document object. This object has
+// properties and functions that we use to access our HTML elements which
+// we can manipulate with JavaScript.
+
+//How to access elements in the DOM
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
