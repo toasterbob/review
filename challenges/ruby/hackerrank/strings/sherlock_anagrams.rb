635882d10e9a1bfc46224cc1a3eadb1acd6355a4
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-04 22:05:35 -0700

    sherlock and anagrams

--- challenges/ruby/hackerrank/strings/sherlock_anagrams.rb
+++ challenges/ruby/hackerrank/strings/sherlock_anagrams.rb
@@ -1,11 +1,14 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 def pairs(str)
-    result = []
-    str.split("").uniq.each do |el|
-        x = str.count(el)
-        result << x if x > 1
+    count = 0
+    hash = Hash.new { |h, k| h[k] = []}
+    str.split("").each_with_index do |el, i|
+        hash[el] << i
     end
-    result.empty? ? 0 : result.reduce(1, :*)
+
+    hash = hash.keep_if { |k, v| v.length > 1}
+    p hash
+    #result.empty? ? 0 : result.reduce(1, :*)
 end
 
 n = gets.strip.to_i
