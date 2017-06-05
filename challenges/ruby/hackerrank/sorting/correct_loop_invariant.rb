a2dcaf74f6055ef239518eb209654b99576003ea
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-05 13:04:46 +0530

    correctness and the loop invariant

--- challenges/ruby/hackerrank/sorting/correct_loop_invariant.rb
+++ challenges/ruby/hackerrank/sorting/correct_loop_invariant.rb
@@ -4,11 +4,13 @@ def  insertionSort(ar)
         if (ar[i] < ar[i - 1])
             value = ar[i]
             j = i-1
-            while (j > 0 && value < ar[j])
+            while (j >= 0 && value < ar[j])
+
                 ar[j+1] = ar[j]
                 j -= 1
             end
-            ar[j+1] = value
+            ar[j + 1] = value
+
         end
         i += 1
     end
