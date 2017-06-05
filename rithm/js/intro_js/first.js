47cc4119364c71dc66a49a679195623b632e94d9
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-04 19:47:17 -0700

    first.js

--- rithm/js/intro_js/first.js
+++ rithm/js/intro_js/first.js
@@ -3,3 +3,17 @@ alert("JS is cool"); //alert doesn't work in node
 let cheese = prompt("Do you like cheese?");
 let result = confirm("You are dumb!"); // true or false return via cancel or okay
 console.log("This is less obtrusive. Cheese = ", cheese, result);
+/* this is the start of
+a multiline comment, and
+this is the ending. */
+
+/*
+JavaScript has 6 primitive data types, but we'll only talk about 5 of them.
+string - var greeting = "hello";
+number - var favoriteNum = 33;
+boolean - var isAwesome = true;
+undefined - var foo; or var setToUndefined = undefined;
+null - var empty = null;
+
+the other is a symbol 
+*/
