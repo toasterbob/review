af21c87abc365d142bd2609cf814a24bf6e3b383
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-04 20:01:44 -0700

    first.js

--- rithm/js/intro_js/first.js
+++ rithm/js/intro_js/first.js
@@ -1,8 +1,8 @@
 alert("JS is cool"); //alert doesn't work in node
 //about:blank  - when using the console
-let cheese = prompt("Do you like cheese?");
+let color = prompt("What's your favorite color?");
 let result = confirm("You are dumb!"); // true or false return via cancel or okay
-console.log("This is less obtrusive. Cheese = ", cheese, result);
+console.log("This is less obtrusive. Color = ", color, " boolean=", result);
 /* this is the start of
 a multiline comment, and
 this is the ending. */
@@ -15,5 +15,34 @@ boolean - var isAwesome = true;
 undefined - var foo; or var setToUndefined = undefined;
 null - var empty = null;
 
-the other is a symbol 
+the other is a symbol
 */
+
+typeof ""; //- "string"
+typeof 5; //- "number"
+typeof false; //- "boolean"
+typeof Symbol(); //- "symbol"
+typeof undefined; //- "undefined"
+typeof null; // hmmm, this is not what we expect, it returns "object"!
+
+parseInt("2"); // 2
+parseFloat("2"); // 2
+parseInt("3.14"); // 3
+parseFloat("3.14"); // 3.14
+parseInt("2.3alkweflakwe"); // 2
+parseFloat("2.3alkweflakwe"); // 2.3
+parseInt("w2.3alkweflakwe"); // NaN (not a number)
+parseFloat("w2.3alkweflakwe"); // NaN (not a number)
+//A nice shorthand for this conversion is the unary operator +:
++"2"; // 2
++"3.14"; // 3.14
++"2.3alkweflakwe"; // NaN
++"w2.3alkweflakwe"; // NaN
+
+
+//Converting to a boolean: !!
+var greeting = "hi";
+var nothing = 0;
+
+!!greeting; // true
+!!nothing; // false
