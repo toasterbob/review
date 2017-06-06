665119c1a49ef6cca1673f75a83400832140baff
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-05 21:54:37 -0700

    sherlock and anagrams

--- challenges/ruby/hackerrank/strings/sherlock_anagrams.rb
+++ challenges/ruby/hackerrank/strings/sherlock_anagrams.rb
@@ -29,3 +29,29 @@ n.times do
     str = gets.strip
     puts pairs(str)
 end
+
+###########################################
+#refactor 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def pairs(str)
+    hash = Hash.new(0)
+
+    0.upto(str.length - 1) do |i|
+        (i).upto(str.length - 1) do |j|
+            sub_str = str[i..j].split("").sort.join("")
+            hash[sub_str] += 1
+        end
+    end
+
+    #hash = hash.keep_if { |k, v| v > 1}
+    total = 0
+
+    hash.values.reduce(0) { |sum, v| sum + (v * (v - 1)/2)}
+end
+
+n = gets.strip.to_i
+
+n.times do
+    str = gets.strip
+    puts pairs(str)
+end
