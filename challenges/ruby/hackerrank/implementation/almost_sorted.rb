6657c5d38be1bb151993e8fd76b7dd08b7848da6
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-06 00:18:59 -0700

    almost sorted

--- challenges/ruby/hackerrank/implementation/almost_sorted.rb
+++ challenges/ruby/hackerrank/implementation/almost_sorted.rb
@@ -24,3 +24,33 @@ end
 gets
 arr = gets.strip.split(" ").map(&:to_i)
 puts almost_sorted(arr)
+
+################################################################
+#refactor
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def almost_sorted(arr)
+    sorted = arr.sort
+    return "yes" if arr == sorted
+
+    i = 0
+    until arr[i] != sorted[i] do
+        i += 1
+    end
+
+    j = -1
+    until arr[j] != sorted[j] do
+        j -= 1
+    end
+    j += arr.length
+
+    compare = arr[0..i - 1] + arr[i..j].reverse + arr[j + 1..-1]
+    arr[i], arr[j] = arr[j], arr[i]
+
+    return "yes\nswap #{i + 1} #{j + 1}" if arr == sorted
+    sorted == compare ? "yes\nreverse #{i + 1} #{j + 1}" : "no"
+
+end
+
+gets
+arr = gets.strip.split(" ").map(&:to_i)
+puts almost_sorted(arr)
