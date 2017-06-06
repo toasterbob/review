92c3900202203f574e8087db2f46462a3bfb2c71
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-06 15:58:25 -0700

    JS arrays

--- rithm/js/intro_js/array.js
+++ rithm/js/intro_js/array.js
@@ -28,3 +28,42 @@ arr; // [5, undefined, 3, 2]
 //the starting index, indicating where values will be removed or added.
 //The second parameter is the number of values to remove.
 //The splice method always returns an array of the removed elements.
+var arr = [1,2,3,4];
+arr.splice(0,1); // returns [1]
+arr; // [2,3,4]
+
+var arr = [1,2,3,4];
+arr.splice(0,1,5); // returns [1]
+arr; // [5,2,3,4]
+
+var arr = ["a","b","c","d"];
+arr.splice(1,2,"x","y","z"); // ["b", "c"]
+arr; // ["a", "x", "y", "z", "d"]
+
+var arr = ["a","b","c","d"];
+arr.splice(4,0,"x","y","z"); // []
+arr; // ["a", "b", "c", "d", "x", "y", "z"]
+
+var faveFoods = ["steak", "fries", "cheese", "popcorn", "pizza"];
+faveFoods[1]; //fries
+faveFoods[4] = "hamburgers";
+faveFoods; //["steak", "fries", "cheese", "popcorn", "hamburgers"]
+var formerFave = faveFoods.splice(0,1);
+faveFoods.push("burritos");
+faveFoods.unshift("filet mignon");
+faveFoods;
+
+[].pop(); //undefined
+
+var arr = [2, 3, 4, 5];
+arr.splice(1,1); //[3]
+arr; //[2, 4, 5]
+
+var arr2 = ["alpha", "gamma", "delta"];
+arr2.splice(1, 0, "beta"); //[]
+arr2; //["alpha", "beta", "gamma", "delta"]
+
+
+var arr3 = [10,-10,-5,-3,2,1];
+arr3.splice(0, 4, 10, 9, 8, 7, 6, 5, 4, 3); //[10, -10, -5, -3]
+arr3; // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
