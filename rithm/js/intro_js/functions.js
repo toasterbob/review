077b249f72567ef1f1da22d4279a7ef86622b772
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-07 11:41:35 -0700

    Functions.js

---
+++ rithm/js/intro_js/functions.js
@@ -0,0 +1,86 @@
+//A function is a repeatable process or procedure.
+
+function anyNameYouWantForTheFunction() {
+    // As many lines of code as you want
+}
+
+// The function keyword,
+// The name of the function (in this case, anyNameYouWantForTheFunction),
+// Any parameters for the function (we'll ignore this for now, but parameters will go inside of the parentheses after the function name),
+// The function body (the code for the function, which lives inside of the curly braces).
+
+function myFunction() {
+
+}
+
+function myOtherFunction() {
+
+}
+
+function yetAnotherFunction() {
+
+}
+
+function okayIGetIt() {
+
+}
+
+function firstFunction() {
+  console.log("I just wrote a function!");
+}
+firstFunction(); //I just wrote a function! - undefined
+
+function firstFunction() {
+  return "I just wrote a function!";
+} //I just wrote a function! - no undefined
+
+function isOverPointFive(){
+    if(Math.random() > .5){
+        return true;
+    }
+    return false;
+}
+//#refactor
+function isOverPointFive(){
+    return Math.random() > .5 ? true : false;
+}
+//#refactor
+function isOverPointFive(){
+    return Math.random() > .5;
+}
+
+function myName(){
+  let name = "Mark Noizumi";
+  console.log(name);
+}
+myName(); //Mark Noizumi
+
+let favoriteFoods = ["pizza", "ice cream"];
+
+function randomFood(favoriteFoods) {
+  return Math.random() > .5 ? favoriteFoods[0] : favoriteFoods[1];
+}
+//Refactor
+// lets find a random number between 0 and 1 and multiply it by the
+// length of the array. This will give us a number between 0 and 2. If
+// we always round down, we will get either 0 or 1, so we can use
+// Math.floor to round down.
+function randomFood(favoriteFoods) {
+  let rand = Math.floor(Math.random() * favoriteFoods.length);
+  return favoriteFoods[rand];
+}
+randomFood(favoriteFoods);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
