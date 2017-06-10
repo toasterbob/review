44a8e8f3e66131ad45706a4ab4b9610cca717d88
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-10 15:25:00 -0700

    Nested Objects

--- rithm/js/intermediate1/nested_objects.js
+++ rithm/js/intermediate1/nested_objects.js
@@ -79,7 +79,7 @@ var instructorData = {
             citiesLivedIn: ["Toledo", "Tokyo", "Indio", "San Diego", "Moon Lake", "New York"]
         }
     }
-}
+};
 
 instructorData.name; // "Elie"
 instructorData.additionalData.instructor; // true
@@ -90,12 +90,73 @@ instructorData.additionalData.moreDetails.citiesLivedIn[1]; // "Providence"
 
 
 
+// Using the dot notation is a great way to access values in nested
+// objects. However, when dynamically setting values in a nested object
+// (when you don't know exactly what the name of the key is going to be),
+// you have to use the bracket notation.
+var programmingLanguages = {
+    java: {
+        yearCreated: 1995,
+        creator: "James Gosling"
+    },
+    javaScript: {
+        yearCreated: 1995,
+        creator: "Brendan Eich"
+    }
+}
+// Remember, that when adding keys to an object, if you do NOT know exactly
+// what the name of the key will be (meaning that the key will be
+// dynamically created), you HAVE to use the bracket notation.
+function addProgrammingLanguage(nameOfLanguage, yearLanguageCreated, creatorOfLanguage){
+    // how can we access the programming languages object?
+    // We can't use dot notation, because we don't know the name of
+    // the key until the function is called.
+    // Instead we use bracket notation where the key is the
+    // nameOfLanguage that is being passed to the function.
+    programmingLanguages[nameOfLanguage] = {
+        // Creating the object which will be the value of the
+        // key.  We can directly assign the yearLanguageCreated
+        // and creatorOfLanguage to the appropriate keys here.
+        yearCreated: yearLanguageCreated,
+        creator: creatorOfLanguage
+    }
+}
 
+// Usage example: Adding a key of ruby to the programming language object,
+// with the value of 1995 for the key "yearCreated" and the value
+// "Yukihiro Matsumoto" for creatorOfLanguage
+addProgrammingLanguage("ruby", 1995, "Yukihiro Matsumoto");
 
+//Writing functions to produce values in a nested data structure
+//When you're interacting with a nested data structure, sometimes it's
+//helpful to encapsulate some of your logic into a function.
 
+var instructorData = {
+    name: "Elie",
+    additionalData: {
+        instructor: true,
+        favoriteHobbies: ["Playing Cello", "Tennis", "Coding"],
+        moreDetails: {
+            favoriteBasketballTeam: "New York Knicks",
+            numberOfSiblings: 3,
+            isYoungest: true,
+            hometown: {
+                city: "West Orange",
+                state: "NJ",
+            },
+            citiesLivedIn: ["Seattle", "Providence", "New York"]
+        }
+    }
+};
 
-
-
+//Exercises
+function displayCities(){
+  let cities = instructorData.additionalData.citiesLivedIn
+  for(let i = 0; i < cities.length; i++){
+    console.log(cities[i]);
+  }
+}
+displayCities();
 
 
 
