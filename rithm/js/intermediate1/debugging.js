a44b2f5a2b5931057944f7ca676021a0733052cc
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-10 00:19:45 -0700

    js errors

--- rithm/js/intermediate1/debugging.js
+++ rithm/js/intermediate1/debugging.js
@@ -2,7 +2,8 @@
 //All errors in JavaScript are actually objects that are created by a
 //function called Error
 
-
+//you can also create and throw your own errors in applications that
+//you write (this is what is meant by a "user-defined exception").
 
 
 
