298a8b8ea2f29f25dafc31aa8f55626f7ff90c44
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-11 12:37:30 -0700

    asynchronous and event loop

--- rithm/js/intermediate1/timers.js
+++ rithm/js/intermediate1/timers.js
@@ -95,12 +95,21 @@ console.log("third"); //"first", "third", "second."
 
 // Exercises
 
+//What is the difference between setInterval and setTimeout?
+//setInterval repeats - setTimeout is one time
 
+// What is the difference between using setInterval and a loop? Why would
+// you want to choose one over the other?
+// setinterval repeats a function at a set amount of time.  loop just repeats.
 
+//What is the first parameter that setInterval and setTimeout accept?
+// they accept a function and then a time in milliseconds
 
+// Why is it so important to store the result of setInterval and setTimeout in a variable?
+// In order to clear them later on
 
-
-
+// What does asynchronous mean in the context of setTimeout and setInterval?
+// they are put into the queue then they are called when the stack is cleared 
 
 
 //
