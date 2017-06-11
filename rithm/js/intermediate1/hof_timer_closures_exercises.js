e890fd623e038bcc16dc0f55de2f2ae6ad4dbded
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-11 16:41:19 -0700

    Higher Order Functions, Timers, and Closures Exercises.

--- rithm/js/intermediate1/hof_timer_closures_exercises.js
+++ rithm/js/intermediate1/hof_timer_closures_exercises.js
@@ -33,17 +33,46 @@ function randomGame(){
 
 ////////////////////////////////////////////////////////////////////////
 
+function isEven(num){
+  return num % 2 === 0
+}
 
+console.log(isEven(2)); // true
+console.log(isEven(3)); // false
 
-
-
-
-
+function isOdd(num){
+  return num % 2 === 1
+}
+console.log(isOdd(3)); // true
+console.log(isOdd(14)); // false
 ////////////////////////////////////////////////////////////////////////
 
+function isPrime(num){
+  if(num < 2){
+    return false;
+  } else if(num === 2){
+    return true;
+  } else {
+    for(let i = num - 1; i >= 2; i--){
+      if(num % i === 0){
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
+isPrime(8); // false
+isPrime(17); // true
 
-
-
+//theirs
+function isPrime(value) {
+    for(var i = 2; i <= Math.sqrt(value); i++) {
+        if(value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
+}
 
 //
