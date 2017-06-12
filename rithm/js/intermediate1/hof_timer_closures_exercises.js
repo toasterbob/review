8961a9e5a4718a148b08746eed6157f576a6b888
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-11 19:30:54 -0700

    Higher Order Functions, Timers, and Closures Exercises.

--- rithm/js/intermediate1/hof_timer_closures_exercises.js
+++ rithm/js/intermediate1/hof_timer_closures_exercises.js
@@ -75,4 +75,70 @@ function isPrime(value) {
     return value > 1;
 }
 
+////////////////////////////////////////////////////////////////////////
+function numberFact(num, fn){
+  return fn(num);
+}
+
+numberFact(59,isEven); // false
+numberFact(59,isOdd); // true
+numberFact(59,isPrime); // true
+
+////////////////////////////////////////////////////////////////////////
+function findIndex(arr, fn){
+  for(let i = 0; i < arr.length; i++){
+    if(fn(arr[i])){
+      return i
+    }
+  }
+}
+// returns 1 (index of the first value greater than or equal to 10)
+findIndex([8,11,4,27], function(val){return val >= 10});
+
+findIndex([8,11,4,27], function(val){return val === 7}); // undefined
+
+
+////////////////////////////////////////////////////////////////////////
+
+function find(arr, fn){
+  for(let i = 0; i < arr.length; i++){
+    if(fn(arr[i])){
+      return arr[i]
+    }
+  }
+}
+find([8,11,4,27], function(val){return val >= 10}); // 11
+find([8,11,4,27], function(val){return val === 5}); // undefined
+//
+function specialMultiply(num){
+  let answer = 1;
+  if (arguments.length > 1){
+    for(let i = 0; i < arguments.length; i++){
+      answer *= arguments[i];
+    }
+    return answer;
+  } else {
+    return function(num2){
+      return num * num2;
+    };
+  }
+}
+
+specialMultiply(3,4); // 12
+specialMultiply(3)(4); // 12
+specialMultiply(3); // returns a function
+
+specialMultiply(3,4,5); // 60 - mine works with this, theirs does not 
+
+//theirs
+function specialMultiply(a,b){
+  if(arguments.length === 1){
+    return function(b){
+      return a*b;
+    }
+  }
+  return a*b;
+}
+
+
 //
