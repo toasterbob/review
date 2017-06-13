161e831baa607901f128760b947f891b1f33cd41
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-12 22:30:43 -0700

    quicksort in-place

--- challenges/ruby/hackerrank/sorting/quicksort_inplace.rb
+++ challenges/ruby/hackerrank/sorting/quicksort_inplace.rb
@@ -36,8 +36,8 @@ puts (inplace_quicksort(arr[0...partition]) + [arr[partition]] + inplace_quickso
 ##############################################
 #refactor
 def inplace_quicksort(arr, start, finish)
-    return arr if finish - start <= 1
-    pivot = arr[arr.length - 1]
+    return arr if finish - start < 1
+    pivot = arr[finish]
     partition = start
     start.upto(finish) do |i|
         if arr[i] <= pivot
@@ -46,7 +46,7 @@ def inplace_quicksort(arr, start, finish)
         end
     end
     puts arr.join(" ")
-    arr = inplace_quicksort(arr, 0, partition - 1)
+    arr = inplace_quicksort(arr, start, partition - 1)
     arr = inplace_quicksort(arr, partition + 1, finish)
     arr
 end
@@ -54,26 +54,3 @@ end
 gets
 arr = gets.chomp.split(" ").map(&:to_i)
 inplace_quicksort(arr, 0, arr.length - 1)
-##############################################
-def quicksort_in_place arr, start, nd
-    if nd - start <= 0
-        return arr
-    end
-    pivot = arr[nd]
-    index = start
-    (start..nd-1).each do |i|
-        if arr[i] <= pivot
-            arr[i], arr[index] = arr[index], arr[i]
-            index += 1
-        end
-    end
-    arr[index], arr[nd] = arr[nd], arr[index]
-    puts arr.join(' ')
-    arr = quicksort_in_place(arr, start, index-1)
-    arr = quicksort_in_place(arr, index+1, nd)
-    arr
-end
-
-n = gets.strip.to_i
-arr = gets.strip.split(' ').map {|i| i.to_i}
-quicksort_in_place arr, 0, n-1
