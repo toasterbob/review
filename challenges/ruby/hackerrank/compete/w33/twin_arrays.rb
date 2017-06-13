bb99d0a3f9b5c99dff615b7b6dcf0b12710fbd88
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-12 23:53:12 -0700

    twin arrays

--- challenges/ruby/hackerrank/compete/w33/twin_arrays.rb
+++ challenges/ruby/hackerrank/compete/w33/twin_arrays.rb
@@ -19,31 +19,28 @@ ar2 = gets.strip.split(' ').map(&:to_i)
 puts twinArrays(ar1, ar2)
 
 #refactor
-#!/bin/ruby
 
 def twinArrays(arr1, arr2)
     min1 = arr1[0]
     sec1 = nil
     min2 = arr2[0]
-    sec 2 = nil
+    sec2 = nil
 
     h1 = Hash.new
     h2 = Hash.new
     arr1.each_with_index do |el, i|
         h1[el] = i
-        if el < min1
+        min1 = el if el < min1
     end
 
-    s_arr1 = arr1.sort
-    s_arr2 = arr2.sort
-    m1 = s_arr1[0]
-    m2 = s_arr2[0]
-    idx1 = arr1.index(m1)
-    idx2 = arr2.index(m2)
-    return m1 + m2 if idx1 != idx2
-    mm1 = s_arr1[1]
-    mm2 = s_arr2[1]
-    [m1 + mm2, m2 + mm1].min
+    arr2.each_with_index do |el, i|
+        h2[el] = i
+        min2 = el if el < min2
+    end
+
+    return min1 + min2 if h1[min1] != h2[min2]
+
+    [min1 + sec2, min2 + sec1].min
 end
 
 n = gets.strip.to_i
