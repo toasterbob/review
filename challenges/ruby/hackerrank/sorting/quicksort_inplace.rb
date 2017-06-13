3d180bee8fed6393da2cd641e65e6175428a07cd
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-12 18:01:42 -0700

    quicksort in-place

--- challenges/ruby/hackerrank/sorting/quicksort_inplace.rb
+++ challenges/ruby/hackerrank/sorting/quicksort_inplace.rb
@@ -1,3 +1,4 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 def inplace_quicksort_stupid(arr) #stupid version just to get the proper output to pass this dumb challenge
     return arr if arr.length <= 1
     pivot = arr[arr.length - 1]
@@ -11,7 +12,23 @@ def inplace_quicksort_stupid(arr) #stupid version just to get the proper output 
     [arr, partition]
 end
 
+def inplace_quicksort(arr) #real version
+    return arr if arr.length <= 1
+    pivot = arr[arr.length - 1]
+    partition = 0
+    0.upto(arr.length - 1) do |i|
+        if arr[i] <= pivot
+            arr[i], arr[partition] = arr[partition], arr[i] if i > partition
+            partition += 1 unless i == arr.length - 1
+        end
+    end
+    inplace_quicksort(arr[0...partition]) + [arr[partition]] + inplace_quicksort(arr[partition + 1..-1])
+end
 
 gets
 arr = gets.chomp.split(" ").map(&:to_i)
-inplace_quicksort(arr)
+a = inplace_quicksort_stupid(arr)
+puts a[0].join(" ")
+partition = a[1]
+puts (inplace_quicksort(arr[0...partition]) + [arr[partition]] + (arr[partition + 1..-1])).join(" ")
+puts (inplace_quicksort(arr[0...partition]) + [arr[partition]] + inplace_quicksort(arr[partition + 1..-1])).join(" ")
