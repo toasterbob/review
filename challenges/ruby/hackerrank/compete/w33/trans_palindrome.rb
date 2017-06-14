7ee13c556d9cc355b1c5afa077606126a3b9474d
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-14 00:57:58 -0700

    transform to palindrome

--- challenges/ruby/hackerrank/compete/w33/trans_palindrome.rb
+++ challenges/ruby/hackerrank/compete/w33/trans_palindrome.rb
@@ -23,4 +23,48 @@ end
 
 
 arr = gets.strip.split(' ').map(&:to_i)
-p transform
+
+
+i, j = 0, arr.length - 1
+count = 0
+while i <= j
+  if i == j
+      count += 1
+      i += 1
+      j -= 1
+  elsif arr[i] == arr[j] || (transform[arr[i]].include?(arr[j]) || transform[arr[j]].include?(arr[i]))
+      count += 2
+        i += 1
+        j -= 1
+  else
+        if arr[i] == arr[j - 1] || (transform[arr[i]].include?(arr[j - 1]) || transform[arr[j - 1]].include?(arr[i]))
+            j -= 1
+        elsif arr[i + 1] == arr[j] || (transform[arr[i + 1]].include?(arr[j]) || transform[arr[j]].include?(arr[i + 1]))
+            i += 1
+        else
+            j -= 1
+        end
+  end
+end
+
+count2 = 0
+while i <= j
+  if i == j
+      count2 += 1
+      i += 1
+      j -= 1
+  elsif arr[i] == arr[j] || (transform[arr[i]].include?(arr[j]) || transform[arr[j]].include?(arr[i]))
+      count2 += 2
+        i += 1
+        j -= 1
+  else
+        if arr[i] == arr[j - 1] || (transform[arr[i]].include?(arr[j - 1]) || transform[arr[j - 1]].include?(arr[i]))
+            j -= 1
+        elsif arr[i + 1] == arr[j] || (transform[arr[i + 1]].include?(arr[j]) || transform[arr[j]].include?(arr[i + 1]))
+            i += 1
+        else
+            i += 1
+        end
+  end
+end
+puts [count, count2].max
