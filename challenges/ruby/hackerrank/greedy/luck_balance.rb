be951b780895aa650f3a39209ca3cf4c2558059e
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-14 15:27:31 -0700

    luck balance

--- challenges/ruby/hackerrank/greedy/luck_balance.rb
+++ challenges/ruby/hackerrank/greedy/luck_balance.rb
@@ -1,15 +1,16 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
 n, k = gets.strip.split(" ").map(&:to_i)
-important, who_cares = [], []
+important, who_cares, win = [], [], []
+
 n.times do
     i, j = gets.strip.split(" ").map(&:to_i)
     important << i if j == 1
     who_cares << i if j == 0
 end
+
 important.sort!
-win = []
+
 (important.length - k).times do
     win << important.shift
 end
 
-puts important.reduce(:+) + who_cares.reduce(:+) - win.reduce(:+)
+puts important.reduce(0, :+) + who_cares.reduce(0, :+) - win.reduce(0, :+)
