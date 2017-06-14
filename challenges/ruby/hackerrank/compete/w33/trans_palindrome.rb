70d4e5870ec83589d0c41425125d9ffb7515bd9a
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-14 00:19:21 -0700

    transform to palindrome

--- challenges/ruby/hackerrank/compete/w33/trans_palindrome.rb
+++ challenges/ruby/hackerrank/compete/w33/trans_palindrome.rb
@@ -1,12 +1,21 @@
 #!/bin/ruby
 
 n,k,m = gets.strip.split(' ').map(&:to_i)
-
 transform = Hash.new {|h, k| h[k] = []}
+
 for a0 in (0..k-1)
     x,y = gets.strip.split(' ').map(&:to_i)
+    transform[x] += transform[y]
+    transform[y] += transform[x]
+
     transform[x].push(y)
     transform[y].push(x)
+
+    transform[x].each {|el| transform[el].push(y)}
+    transform[y].each {|el| transform[el].push(x)}
+
+    transform[x] = transform[x].uniq
+    transform[y] = transform[y].uniq
 end
-a = gets.strip
-a = a.split(' ').map(&:to_i)
+
+arr = gets.strip.split(' ').map(&:to_i)
