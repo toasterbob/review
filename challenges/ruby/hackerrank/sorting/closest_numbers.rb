b3d2145feb5fc5a2caf1a51761dec97600c92d04
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-13 23:16:29 -0700

    closest numbers

--- challenges/ruby/hackerrank/sorting/closest_numbers.rb
+++ challenges/ruby/hackerrank/sorting/closest_numbers.rb
@@ -1,1 +1,10 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+gets
+arr = gets.strip.split(" ").map(&:to_i)
+arr.sort!
+pairs = [arr[0], arr[1]]
+min = (arr[1] - arr[0]).abs
+
+1.upto(arr.length - 2) do |i|
+    if arr[i + 1] =
+end
