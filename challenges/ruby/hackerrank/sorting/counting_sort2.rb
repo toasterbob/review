7c3f907b37668e2713c38de8f6e5ba712a9316e4
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-13 21:28:55 -0700

    counting sort 2

--- challenges/ruby/hackerrank/sorting/counting_sort2.rb
+++ challenges/ruby/hackerrank/sorting/counting_sort2.rb
@@ -7,3 +7,12 @@ result = []
     hash[i].times {result << i}
 end
 puts result.join(" ")
+
+##########
+gets
+arr = gets.strip.split(" ").map(&:to_i)
+arr2 = Array.new(100, 0)
+arr.each {|el| arr2[el] += 1}
+result = []
+arr2.each_with_index {|el, i| el.times{result << i}}
+puts result.join(" ")
