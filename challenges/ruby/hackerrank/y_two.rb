98363e5a6cc40ce1b038dfa6aa192d9e5b4a49df
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-20 16:31:56 -0700

    strings

---
+++ challenges/ruby/hackerrank/y_two.rb
@@ -0,0 +1,29 @@
+def sort_businesses_by_rating(businesses)
+    
+    hash = Hash.new {|h,k| h[k] = []}
+
+    businesses.each do |h|
+        hash[h[:rating]].push(h)
+    end
+
+    result = []
+    5.downto(1) do |i|
+        result += hash[i]
+    end
+
+    result
+end
+
+
+businesses = []
+ARGF.each_line do |line|
+  businesses.push({
+    id: line.split(' ')[0].to_i,
+    rating: line.split(' ')[1].to_i
+  })
+end
+
+sorted_businesses = sort_businesses_by_rating(businesses)
+sorted_businesses.each do |business|
+  puts "#{business[:id]} #{business[:rating]}"
+end
