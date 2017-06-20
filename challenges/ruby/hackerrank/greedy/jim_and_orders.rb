3110fa613a0a014dcb39f73e56e281c8cf534ada
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-20 14:44:58 -0700

    Jim and the Orders

---
+++ challenges/ruby/hackerrank/greedy/jim_and_orders.rb
@@ -0,0 +1,20 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = gets.strip.to_i
+arr = []
+
+n.times do
+    time = gets.strip.split(" ").map(&:to_i).reduce(:+)
+    arr << time
+end
+
+hash = Hash.new {|h, v| h[v] = []}
+arr.each_with_index {|el, i| hash[el].push(i + 1)}
+
+result = []
+
+hash.keys.sort.each do |key|
+
+    result += hash[key].sort
+end
+
+puts result.join(" ")
