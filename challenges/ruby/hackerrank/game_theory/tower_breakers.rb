390710526395a689365a33487d092d871dab7a5f
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-21 14:25:49 -0700

    tower breakers

--- challenges/ruby/hackerrank/game_theory/tower_breakers.rb
+++ challenges/ruby/hackerrank/game_theory/tower_breakers.rb
@@ -1,6 +1,6 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 def towers(n, m)
-
+    m == 1 || n % 2 == 0 ? 2 : 1
 end
 
 t = gets.strip.to_i
@@ -9,5 +9,3 @@ t.times do
    x, y = gets.strip.split(" ").map(&:to_i)
    puts towers(x, y)
 end
-
-#(m == 1 || n % 2 == 0 ? 2 : 1)
