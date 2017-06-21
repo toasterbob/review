cc076a4c2ff5a2d2b400850a654f6c2d1253a674
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-20 23:55:02 -0700

    permuting two arrays

---
+++ challenges/ruby/hackerrank/game_theory/game_of_thrones.rb
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def stone_game(x)
+
+end
+
+n = gets.strip.to_i
+
+n.times do
+   x = gets.strip_to_i
+   puts stone_game(x)
+end
