22eeebd457f90829d34d4affcecd388a5410bc61
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-20 23:43:08 -0700

    permuting two arrays

--- challenges/ruby/hackerrank/greedy/permuting_two_arrays.rb
+++ challenges/ruby/hackerrank/greedy/permuting_two_arrays.rb
@@ -1,1 +1,18 @@
- 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def two_arrays(arr1, arr2, k)
+   arr1.sort!
+   arr2 = arr2.sort.reverse
+   0.upto(arr1.length - 1) do |i|
+      return "NO" unless arr1[i] + arr2[i] >= k
+   end
+   "YES"
+end
+
+n = gets.strip.to_i
+
+n.times do
+   n, k = gets.strip.split(" ").map(&:to_i)
+   arr1 = gets.strip.split(" ").map(&:to_i)
+   arr2 = gets.strip.split(" ").map(&:to_i)
+   puts two_arrays(arr1, arr2, k)
+end
