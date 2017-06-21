d3d6dafbe79a25aa63baa642b5f4cedefa4492a9
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-21 00:04:25 -0700

    game of thrones

--- challenges/ruby/hackerrank/game_theory/game_of_thrones.rb
+++ challenges/ruby/hackerrank/game_theory/game_of_thrones.rb
@@ -1,13 +1,12 @@
 
 def stone_game(x)
-    return "Second" if x == 1
-
+    return "Second" if x % 7 == 0 || x % 7 == 1
+    "First"
 end
 
 n = gets.strip.to_i
 
 n.times do
-   x = gets.strip_to_i
+   x = gets.strip.to_i
    puts stone_game(x)
 end
- 
