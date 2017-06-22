6283151aa5af484ed5124bcf3c3fc8060746529d
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-22 14:37:12 -0700

    two sum

--- challenges/ruby/leetcode/y/two_sum.rb
+++ challenges/ruby/leetcode/y/two_sum.rb
@@ -5,8 +5,9 @@ def two_sum(arr, sum)
   i = 0
   while i < arr.length
     #return [arr[i], sum - arr[i]] if hash[sum - arr[i]] #return numbers
-    return [i, hash[sum - arr[i]]] if hash[sum - arr[i]] #return indexes
+    return [i, hash[sum - arr[i]]] if hash[sum - arr[i]] && hash[sum - arr[i]] != i#return indexes
     i += 1
   end
   -1
 end
+#returns first pair that matches the condition or -1 if no condition matches
