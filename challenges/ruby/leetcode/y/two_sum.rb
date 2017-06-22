dfc35f9de20dc5f61e200df71c25fde92a77803e
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-22 14:32:42 -0700

    two sum

--- challenges/ruby/leetcode/y/two_sum.rb
+++ challenges/ruby/leetcode/y/two_sum.rb
@@ -1,10 +1,11 @@
 def two_sum(arr, sum)
   hash = Hash.new
-  arr.each { |el| hash[el] = i }
+  arr.each_with_index { |el, i| hash[el] = i }
 
   i = 0
   while i < arr.length
     return [arr[i], sum - arr[i]] if hash[sum - arr[i]]
     i += 1
   end
+  -1
 end
