2c0237d6c18d4512d5537fcdfd260baa6ecee44d
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-21 20:19:41 -0700

    chatbots

--- devBootcamp/chatbots.rb
+++ devBootcamp/chatbots.rb
@@ -6,7 +6,41 @@
 
 #cooperative conversation - paul grice
 
+#turn taking, threading(certain bits persist), anticipation, efficacy, trust
+#preparing for failure - rescuing, non-condescendence, validation
 
+# 7 I's chatbot
+# -interest
+# -interface
+# -input
+# -intent
+# -interaction
+# -implementation
+# -improvement
+
+#Pusheen Pizza Bot
+
+#Recipe
+# full-duplex connection
+# client/consumer
+# server/computation
+# natural language engine
+#
+# optional
+# workers
+# database
+
+# half-duplex - abiity to communicate with one message - http - call and response - only one
+#can communicate at once
+# full-duplex - web cocket protocol -many clients at once - messages can be sent at once
+
+# rails conventions + web sockets = ActionCable
+
+# natural language engines - api.ai/google, amazon lex, ibm watson, wit.AI
+# Wit.AI is free
+
+ # Wit comes with a ruby gem - wit-ruby
+ 
 
 
 
