e68e635ecb9d5c4894e22db2366822ba155ecedb
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-22 18:19:27 -0700

    two sum

--- challenges/ruby/leetcode/y/two_sum.rb
+++ challenges/ruby/leetcode/y/two_sum.rb
@@ -1,15 +1,16 @@
 def two_sum(arr, sum)
   hash = Hash.new { |h, v| h[v] = [] }
-  arr.each_with_index { |el, i| hash[el].push[i] }
-
+  arr.each_with_index { |el, i| hash[el].push(i) }
 
   i = 0
   while i < arr.length
     #return [arr[i], sum - arr[i]] if hash[sum - arr[i]] #return numbers
-    return [i, hash[sum - arr[i]].first] if hash[sum - arr[i]] && hash[sum - arr[i].first] != i#return indexes
+    return [i, hash[sum - arr[i]][0]] if hash[sum - arr[i]][0] && hash[sum - arr[i]][0] != i#return indexes
     i += 1
   end
   -1
 end
 #returns first pair that matches the condition or -1 if no condition matches
 #need to check that index doesn't compare to itself
+a = [1, 2, 3, 4]
+p two_sum(a, 7)
