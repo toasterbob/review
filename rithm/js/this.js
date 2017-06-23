4e96d4bd4efc24e4647591a871185d33a83f898c
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-22 19:19:17 -0700

    this

--- rithm/js/this.js
+++ rithm/js/this.js
@@ -62,6 +62,53 @@ person.sayHiLater = function(){
 
 person.sayHiLater()  // says hi!
 
+var school = {
+  name: "Rithm",
+  makeAd: function(buzzword){
+    return function(adjective){
+      return "You should come to" + this.name + "because it's " + adjective + "! You will learn about " + buzzword;
+    }
+  }
+}
+school.makeAd("the keyword this")("confusing"); //"You should come tobecause it's confusing! You will learn about the keyword this"
+
+// arrow functions have lexical binding
+var school = {
+  name: "Rithm",
+  makeAd: function(buzzword){
+    return (adjective) => {
+      return "You should come to " + this.name + " because it's " + adjective + "! You will learn about " + buzzword;
+    }
+  }
+}
+school.makeAd("the keyword this")("confusing"); // works
+
+var person = {
+  name: "Matt",
+  sayHi: function() {
+    return this.name + " says hi!";
+  },
+  dog: {
+    name: "Whiskey",
+    bark: function(){
+      return this.name + " says woof!";
+
+    }
+  }
+};
+person.dog.bark.call(person); //"Matt says woof!"
+//explicit value takes precedence over implicit binding
+
+person.dog.bark.apply(person); // "Matt says woof!"
+person.dog.bark.bind(person)(); //"Matt says woof!"
+
+whatIsThis.call(person)
+whatIsThis.apply(person.dog)
+whatIsThis.call(213)
+
+// apply always takes at most two arguments (thisArg, [])
+// call rather than passing in an array (call can take unlimited args)
+
 
 
 
