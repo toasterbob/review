71c1f27b8857e577695430cb08766f00dabc8cd1
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-25 15:12:24 -0700

    ruby objects cycle through

--- tech/classes.rb
+++ tech/classes.rb
@@ -1,7 +1,12 @@
 class Pokemon
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
 end
 
-pikatchu = Pokemon.new
-charmander = Pokemon.new
+pikachu = Pokemon.new("pikachu")
+charmander = Pokemon.new("charmander")
 
-ObjectSpace.each_object(Pokemon) {|x| p x}
+ObjectSpace.each_object(Pokemon) {|x| puts x.name}
