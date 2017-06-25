b27a941538459ca737e0b455344db3d6ac4ed8e5
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-25 12:20:21 -0700

    frontend

--- tech/backend.js
+++ tech/backend.js
@@ -11,3 +11,21 @@ function add(x, y){
 
 add(2, 5); // 7
 add(2)(5); // 7
+
+//refactor 
+function add(x, ...yArgs){
+  if (yArgs.length > 0){
+    let total = x;
+    for(let i = 0; i < yArgs.length; i++){
+      total += yArgs[i];
+    }
+    return total;
+  } else {
+    return function(y){
+      return x + y;
+    };
+  }
+}
+add(2, 5, 9); // 16
+add(2, 5); // 7
+add(2)(5); // 7
