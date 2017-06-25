4d9402024f52c463f26f3651c0a4ab0052f47ca6
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-25 11:08:11 +0530

    Polytree #parent= which sets parent and children

--- ruby/day5/polytree_node/lib/00_tree_node.rb
+++ ruby/day5/polytree_node/lib/00_tree_node.rb
@@ -10,7 +10,26 @@ class PolyTreeNode
 
   def parent=(parent)
     @parent = parent
-    parent.children << self 
+    parent.children << self
   end
 
+end
+
+if __FILE__ == $PROGRAM_NAME
+  n1 = PolyTreeNode.new("root1")
+  n2 = PolyTreeNode.new("root2")
+  n3 = PolyTreeNode.new("root3")
+
+  # connect n3 to n1
+  n3.parent = n1
+  # connect n3 to n2
+  n3.parent = n2
+
+  # this should work
+  raise "Bad parent=!" unless n3.parent == n2
+  raise "Bad parent=!" unless n2.children == [n3]
+
+  # this probably doesn't
+  n1.children.each { |child| puts child.value }
+  raise "Bad parent=!" unless n1.children == []
 end
