6015950361be005c63a27d2261cb9641bc9417f8
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-26 18:12:48 -0700

    trie

--- ruby/data_structures/trie/trie.rb
+++ ruby/data_structures/trie/trie.rb
@@ -8,3 +8,28 @@ puts DICTIONARY.has_key?("abds").nil? #not in dictionary, returns nil
 puts DICTIONARY.has_key?("she").nil? #false
 puts DICTIONARY.has_key?("she") #true
 puts DICTIONARY.has_key?("he") #true
+
+
+
+# Performance Characteristics
+#
+# Here are some quick benchmarks on my 2.4ghz Intel Core 2 Duo MacBook Pro:
+# For keys that are 5 characters long:
+# 31,344 adds/second
+# 1,827,408 searches/second
+# 38,453 prefixes searches/second
+#
+# For keys that are 10 characters long:
+# 30,653 adds/second
+# 1,802,649 searches/second
+# 13,553 prefix searches/second
+#
+# For keys that are 20 characters long:
+# 30,488 adds/second
+# 1,851,461 searches/second
+# 5,855 prefix searches/second
+#
+# For keys that are 40 characters long:
+# 30,710 adds/second
+# 1,838,380 searches/second
+# 2,762 prefix searches/second
