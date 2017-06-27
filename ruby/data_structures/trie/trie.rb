0352dde69e380a1dc8b4400d49c8097e18f81db7
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-26 18:02:32 -0700

    trie

--- ruby/data_structures/trie/trie.rb
+++ ruby/data_structures/trie/trie.rb
@@ -3,6 +3,6 @@ require "trie"
 DICTIONARY = Trie.new
 File.readlines('words.txt').each { |line| DICTIONARY.add(line.strip) }
 
-puts DICTIONARY.has_key?("shell")
+puts DICTIONARY.has_key?("hell")
 puts DICTIONARY.has_key?("she")
-puts DICTIONARY.has_key?("she")
+puts DICTIONARY.has_key?("he")
