1d4247d860e9b04c32ea680240d590a3ff96b7d4
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-26 18:06:52 -0700

    trie

--- ruby/data_structures/trie/trie.rb
+++ ruby/data_structures/trie/trie.rb
@@ -3,6 +3,8 @@ require "trie"
 DICTIONARY = Trie.new
 File.readlines('words.txt').each { |line| DICTIONARY.add(line.strip) }
 
-puts DICTIONARY.has_key?("hell")
-puts DICTIONARY.has_key?("she")
-puts DICTIONARY.has_key?("he")
+puts DICTIONARY.has_key?("abds").nil? #not in dictionary, returns nil
+
+puts DICTIONARY.has_key?("she").nil? #false
+puts DICTIONARY.has_key?("she") #true
+puts DICTIONARY.has_key?("he") #true
