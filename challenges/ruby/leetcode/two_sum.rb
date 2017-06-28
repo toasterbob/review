fa803bc08446bff46f4eaee07d341ae99ecc9910
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-28 23:14:09 +0530

    two sum

--- challenges/ruby/leetcode/two_sum.rb
+++ challenges/ruby/leetcode/two_sum.rb
@@ -9,3 +9,14 @@ def two_sum(nums, target)
        return [i, hash[goal]] if hash[goal] && hash[goal] != i
     end
 end
+
+####
+### I thought this might optimize it a bit, but not sure.  
+def two_sum(nums, target)
+    hash = Hash.new
+    nums.each_with_index do |num, i|
+       goal = target - num
+       return [hash[goal], i] if hash[goal]
+       hash[num] = i
+    end
+end
