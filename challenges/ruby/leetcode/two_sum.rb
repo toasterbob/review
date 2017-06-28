207f9aa1f481e883a330b9a2b86126081713ac89
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-28 23:16:52 +0530

    two sum

--- challenges/ruby/leetcode/two_sum.rb
+++ challenges/ruby/leetcode/two_sum.rb
@@ -1,6 +1,10 @@
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer[]}
+### each submission gets a slightly different time
+# because of variations the range for the same code can be in the top 97.5%
+# all the way down to 56% FOR THE SAME CODE - at a certain level the variations are
+# noise 
 def two_sum(nums, target)
     hash = Hash.new
     nums.each_with_index { |num, i| hash[num] = i }
@@ -11,7 +15,7 @@ def two_sum(nums, target)
 end
 
 ####
-### I thought this might optimize it a bit, but not sure.  
+### I thought this might optimize it a bit, but not sure.
 def two_sum(nums, target)
     hash = Hash.new
     nums.each_with_index do |num, i|
