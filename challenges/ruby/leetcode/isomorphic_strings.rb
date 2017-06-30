e92b53681651a0423bd8fa63d2e72002220af268
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-30 01:10:36 -0700

    isomorphic strings

--- challenges/ruby/leetcode/isomorphic_strings.rb
+++ challenges/ruby/leetcode/isomorphic_strings.rb
@@ -1,29 +1,30 @@
 # @param {String} s
 # @param {String} t
 # @return {Boolean}
+
 def is_isomorphic(s, t)
     return false if s.length != t.length
-    hash = Hash.new(0)
-    s.split("").each { |ch| hash[ch] += 1 }
-    s = s.split("").map { |ch| "#{ch}#{hash[ch]}" }
-
-    hash2 = Hash.new(0)
-    t.split("").each { |ch| hash2[ch] += 1 }
-    t = t.split("").map { |ch| "#{ch}#{hash2[ch]}" }
-
+    hash = Hash.new
+    hash2 = Hash.new
     i = 0
-    s_string = s.join("")
-    t_string = t.join("")
     while i < s.length
-      if s[i] == t[i]
-          i += 1
-      else
-         t_string.gsub!(t[i], s[i])
-         i += 1
-      end
-    end
+        if hash[s[i]]
+            return false if hash[s[i]] != t[i]
+        else
+            hash[s[i]] = t[i]
 
-    s_string == t_string
+        end
+
+
+        if hash2[t[i]]
+            return false if hash2[t[i]] != s[i]
+        else
+            hash2[t[i]] = s[i]
+        end
+
+        i += 1
+    end
+    true
 end
 
 if __FILE__ == $PROGRAM_NAME
