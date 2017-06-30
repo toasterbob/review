59b39606147543f36eaae2eaba2cb0c18ea074e4
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-29 17:48:10 -0700

    merge intervals

---
+++ challenges/ruby/leetcode/merge_intervals.rb
@@ -0,0 +1,24 @@
+# Definition for an interval.
+# class Interval
+#     attr_accessor :start, :end
+#     def initialize(s=0, e=0)
+#         @start = s
+#         @end = e
+#     end
+# end
+
+# @param {Interval[]} intervals
+# @return {Interval[]}
+def merge(intervals)
+    new_intervals = []
+    i = 0
+    while i < intervals.length - 1
+        j = i + 1
+        while j < intervals.length
+
+            j += 1
+        end
+        i += 1
+    end
+
+end
