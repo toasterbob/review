ced6abe05b090a02da5ea378485964013056b870
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-30 01:17:11 -0700

    isomorphic strings

--- challenges/ruby/leetcode/isomorphic_strings.rb
+++ challenges/ruby/leetcode/isomorphic_strings.rb
@@ -11,15 +11,8 @@ def is_isomorphic(s, t)
         if hash[s[i]]
             return false if hash[s[i]] != t[i]
         else
+            return false is hash.has_value?(t[i])
             hash[s[i]] = t[i]
-
-        end
-
-
-        if hash2[t[i]]
-            return false if hash2[t[i]] != s[i]
-        else
-            hash2[t[i]] = s[i]
         end
 
         i += 1
@@ -29,20 +22,4 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   p is_isomorphic("egg", "add")
-end
-
-
-#########
-def is_isomorphic(s, t)
-    return false unless s.length == t.length
-    map = Hash.new
-    s.chars.each_with_index do |c, i|
-        if map.has_key? c
-            return false if t[i] != map[c]
-        else
-            return false if map.has_value? t[i]
-            map[c] = t[i]
-        end
-    end
-    true
 end
