fb5841faceab12754b6c89d8ae2c04039fb41fe5
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-29 18:09:23 -0700

    merge intervals

--- challenges/ruby/leetcode/merge_intervals.rb
+++ challenges/ruby/leetcode/merge_intervals.rb
@@ -9,16 +9,29 @@
 
 # @param {Interval[]} intervals
 # @return {Interval[]}
+
 def merge(intervals)
     new_intervals = []
     i = 0
     while i < intervals.length - 1
+        temp_intervals = []
+        starts, ends = intervals[i][0], intervals[i][1]
         j = i + 1
         while j < intervals.length
-
+            starts2, ends2 = intervals[j][0], intervals[j][1]
+            if starts2 >= starts && starts2 <= ends
+                ends = ends2 if ends2 > ends
+            elsif starts2 <= starts && ends2 >= starts && ends2 <= ends
+                starts = starts2 if starts2 < starts
+            else
+                temp_intervals << intervals[j]
+            end
             j += 1
         end
+        new_intervals << [starts, ends]
+        new_intervals.concat(temp_intervals)
+        intervals = new_intervals
         i += 1
     end
-
+    intervals
 end
