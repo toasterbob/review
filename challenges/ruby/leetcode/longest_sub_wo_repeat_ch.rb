bf4e11254aaf58b8ba7cf0ef6ad348f8d7f62e92
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-30 13:12:36 -0700

    longest sub without repeating character

--- challenges/ruby/leetcode/longest_sub_wo_repeat_ch.rb
+++ challenges/ruby/leetcode/longest_sub_wo_repeat_ch.rb
@@ -29,7 +29,45 @@ def length_of_longest_substring(s)
     result.length
 end
 
+
+
+
+######
+def length_of_longest_substring2(s)
+    max = 0
+    map = {}
+    left = -1
+    s.chars.each_with_index do |c, i|
+        if ci = map[c] and ci > left
+            left = ci
+        end
+        map[c] = i
+        #puts "#{i},left=#{left},#{map}"
+        new_length = i - left
+        max = new_length if max < new_length
+    end
+    max
+end
+
+def length_of_longest_substring3(s)
+  hash = Hash.new
+  i = 0
+  j = 0
+  max = 0
+  while j < s.length
+    ch = s[j]
+    if hash[ch]
+      i = hash[ch] if hash[ch] > i
+    end
+    max = j - i + 1 if j - i + 1 > max
+    hash[ch] = j + 1
+
+    j += 1
+  end
+  return max
+end
+
 if __FILE__ == $PROGRAM_NAME
-  p length_of_longest_substring("abba")
-  p length_of_longest_substring("abcabcbb")
+  # p length_of_longest_substring("abba")
+  p length_of_longest_substring3("abcabcbb")
 end
