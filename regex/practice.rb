3f75f50ce8984618411007a0f66ac5c8db08ae5c
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-01 12:33:43 -0700

    regex practice

--- regex/practice.rb
+++ regex/practice.rb
@@ -3,4 +3,9 @@ emailRegExp = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/ #email addresses
 
 check = /[a-z]+at/
 words = ["hat", "cot", "cat"]
-words.join(" ").scan(/[a-z]+at/)
+words.join(" ").scan(/[a-z]+a+t/)
+
+check = /h+[a-z]+t/
+    words = ["hat", "cot", "cat"]
+    words.join(" ").scan(/h+[a-z]+t/)
+    begin_word
