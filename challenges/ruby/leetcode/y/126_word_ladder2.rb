77361e2cdb5b40cf2d0f4fe70d6bcbe8da3fa40d
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-01 15:48:48 -0700

    word ladder 2 with hash

--- challenges/ruby/leetcode/y/126_word_ladder2.rb
+++ challenges/ruby/leetcode/y/126_word_ladder2.rb
@@ -2,22 +2,27 @@
 # @param {String} end_word
 # @param {String[]} word_list
 # @return {String[][]}
+
 def check_word(word, word_list)
     #find next step words
-    result = []
-    i = 0
-    while i < word.length
-        check = word.chars
-        check[i] = '[a-z]'
-        check = "#{check.join('+')}"
-        check = Regexp.new check
-        words = word_list.join(" ").scan(check)
+    def run_check(word, word_list)
+      result = []
+      i = 0
+      while i < word.length
+          check = word.chars
+          check[i] = '[a-z]'
+          check = "#{check.join('+')}"
+          check = Regexp.new check
+          words = word_list.join(" ").scan(check)
 
-        result.concat(words)
-        i += 1
+          result.concat(words)
+          i += 1
+      end
+      result.delete(word)
+      @hash[word] = result
     end
-    result.delete(word)
-    result
+
+    @hash[word] ? @hash[word] : run_check(word, word_list)
 end
 
 def process(beginnings, word_list)
@@ -25,27 +30,34 @@ def process(beginnings, word_list)
   beginnings.each do |arr|
     word = arr[-1]
     check_word(word, word_list).each do |result|
-      new_beginnings << (arr.dup << result) unless result.empty?
+      new_beginnings << (arr.dup << result) unless result.empty? || arr.include?(result)
     end
   end
   new_beginnings
 end
 
 def find_ladders(begin_word, end_word, word_list)
+    return [] unless word_list.include?(end_word)
     final_result = []
-    hash = Hash.new
+    @hash = Hash.new
     beginnings = []
     check_word(begin_word, word_list).each do |word|
-      beginnings << [word]
+      beginnings << [begin_word, word]
     end
-    return [[begin_word, end_word]] if beginnings.include?([end_word])
+    return [[begin_word, end_word]] if beginnings.include?([begin_word, end_word])
 
     endings = check_word(end_word, word_list)
     return [] if endings.empty?
+    return [] if beginnings.empty?
+    beginnings.each do |arr|
+      check = arr[-1]
+      final_result << (arr.dup << end_word) if endings.include?(check)
+    end
 
     #one cycle
     while final_result.empty?
       beginnings = process(beginnings, word_list)
+      return [] if beginnings.empty?
       beginnings.each do |arr|
         check = arr[-1]
         final_result << (arr.dup << end_word) if endings.include?(check)
@@ -62,4 +74,5 @@ if __FILE__ == $PROGRAM_NAME
   endWord = "cog"
   wordList = ["hot","dot","dog","lot","log","cog"]
   p find_ladders(beginWord, endWord, wordList)
+  p find_ladders("a", "b", ["a","b","c"])
 end
