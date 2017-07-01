a6ef79f368f16876fc199b00d7ce87d8861d90c6
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-01 00:35:46 -0700

    group anagrams

--- challenges/ruby/leetcode/y/group_anagrams.rb
+++ challenges/ruby/leetcode/y/group_anagrams.rb
@@ -12,3 +12,19 @@ def group_anagrams(strs)
     end
     result
 end
+
+def get_key(str)
+   str.chars.sort.join('')
+end
+
+
+#optimization
+#change split to chars went from 15 to 85th percentile 
+def group_anagrams(strs)
+    hash = Hash.new { |h, k| h[k] = []}
+    strs.each do |str|
+       holder = str.chars.sort.join("")
+       hash[holder] << str
+    end
+    hash.values
+end
