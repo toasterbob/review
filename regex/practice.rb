9441a7bf9de7d123f678f0f98da2688fd02d6ae6
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-01 11:38:31 -0700

    regex practice

--- regex/practice.rb
+++ regex/practice.rb
@@ -1,1 +1,6 @@
-/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/
+emailRegExp = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/ #email addresses
+
+
+check = /[a-z]+at/
+words = ["hat", "cot", "cat"]
+words.join(" ").scan(/[a-z]+at/)
