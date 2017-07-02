b51bf3a7b37178544ffa08e140ad3dac07fe14c0
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-02 15:59:27 -0700

    children tic tac toe node

--- ruby/day5/tic_tac_toe/lib/tic_tac_toe_node.rb
+++ ruby/day5/tic_tac_toe/lib/tic_tac_toe_node.rb
@@ -18,6 +18,17 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
-    
+    game_state_nodes = []
+    0.upto(2) do |i|
+      0.upto(2) do |j|
+        if @board.empty?([i, j])
+          new_board = @board.dup
+          new_board[[i, j]] = @next_mover_mark
+          game_state_nodes << new_board
+        end
+      end
+    end
+
+    game_state_nodes
   end
 end
