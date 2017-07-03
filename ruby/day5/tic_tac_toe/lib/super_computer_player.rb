afa54163b762ca476ac5e77c1ef98d114c1ebe01
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-03 00:48:22 -0700

    super computer move method tic tac toe node

--- ruby/day5/tic_tac_toe/lib/super_computer_player.rb
+++ ruby/day5/tic_tac_toe/lib/super_computer_player.rb
@@ -4,14 +4,16 @@ class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
     ai = TicTacToeNode.new(game.board, mark)
     children = ai.children
-    
+
     children.each do |child|
       return child.prev_move_pos if child.winning_node?(mark)
     end
 
     children.each do |child|
-
+      return child.prev_move_pos if child.losing_node?(mark) == false
     end
+
+    raise "You should never make it here!"
   end
 end
 
