d489118828bdd753cdcbb71b9bba88f0147dc67f
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-03 01:00:09 -0700

    super computer move method tic tac toe node

--- ruby/day5/tic_tac_toe/lib/super_computer_player.rb
+++ ruby/day5/tic_tac_toe/lib/super_computer_player.rb
@@ -5,21 +5,23 @@ class SuperComputerPlayer < ComputerPlayer
     ai = TicTacToeNode.new(game.board, mark)
     children = ai.children
 
+    children.shuffle!
+    #if there is a move that guarantees a win the next play, take it
     children.each do |child|
       return child.prev_move_pos if child.winning_node?(mark)
     end
-
+    #else pick a move that means they won't lose the next turn
     children.each do |child|
       return child.prev_move_pos if child.losing_node?(mark) == false
     end
-
+    #this means they play a perfect game! 
     raise "You should never make it here!"
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Play the brilliant computer!"
-  hp = HumanPlayer.new("Jeff")
+  hp = HumanPlayer.new("Mark")
   cp = SuperComputerPlayer.new
 
   TicTacToe.new(hp, cp).run
