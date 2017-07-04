a0a502e5b87bbc195b74d217a2e61bae3377d57e
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-03 18:00:49 -0700

    word ladder 2 w/ polytree node

--- challenges/ruby/leetcode/y/126_word_poly.rb
+++ challenges/ruby/leetcode/y/126_word_poly.rb
@@ -78,3 +78,70 @@ class PolyTreeNode
   end
 
 end
+
+def check_word(word, word_list)
+    #find next step words
+    def run_check(word, word_list)
+      result = []
+      i = 0
+      while i < word.length
+          check = word.chars
+          check[i] = '[a-z]'
+          check = "#{check.join('+')}"
+          check = Regexp.new check
+          words = word_list.join(" ").scan(check)
+
+          result.concat(words)
+          i += 1
+      end
+      result.delete(word)
+      @hash[word] = result
+    end
+
+    @hash[word] ? @hash[word] : run_check(word, word_list)
+end
+
+def process(beginnings, word_list)
+  new_beginnings = []
+  beginnings.each do |arr|
+    word = arr[-1]
+    check_word(word, word_list).each do |result|
+      new_beginnings << (arr.dup << result) unless result.empty? || arr.include?(result)
+    end
+  end
+  new_beginnings
+end
+
+def find_ladders(begin_word, end_word, word_list)
+    return [] unless word_list.include?(end_word)
+    final_result = []
+    @hash = Hash.new
+
+    
+    check_word(begin_word, word_list).each do |word|
+      beginnings << [begin_word, word]
+    end
+    return [[begin_word, end_word]] if beginnings.include?([begin_word, end_word])
+
+    endings = check_word(end_word, word_list)
+    return [] if endings.empty?
+    return [] if beginnings.empty?
+    beginnings.each do |arr|
+      check = arr[-1]
+      final_result << (arr.dup << end_word) if endings.include?(check)
+    end
+
+    #one cycle
+    while final_result.empty?
+      beginnings = process(beginnings, word_list)
+      return [] if beginnings.empty?
+      beginnings.each do |arr|
+        check = arr[-1]
+        final_result << (arr.dup << end_word) if endings.include?(check)
+      end
+    end
+
+    # p beginnings
+    # p endings
+    final_result
+end
