b1fb5b99cb17c5eb74ba0a7d14c3aaaa15039dd4
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-12 23:40:15 -0700

    tic tac toe

---
+++ javascript/day2/tictactoe/tic_tac_toe/lib/super_computer_player.rb
@@ -0,0 +1,32 @@
+require_relative 'tic_tac_toe_node'
+
+class SuperComputerPlayer < ComputerPlayer
+  def move(game, mark)
+    ai = TicTacToeNode.new(game.board, mark)
+    children = ai.children
+
+    children.shuffle! #so that it doesn't play the same move every time
+    #if there is a move that guarantees a win the next play, take it
+    children.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark)
+    end
+    #alt way to write above 
+    # node = children.find{ |child| child.winning_node?(mark) }
+    # return node.prev_move_pos if node
+
+    #else pick a move that means they won't lose the next turn
+    children.each do |child|
+      return child.prev_move_pos if child.losing_node?(mark) == false
+    end
+    #this means they play a perfect game!
+    raise "You should never make it here!"
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts "Play the brilliant computer!"
+  hp = HumanPlayer.new("Mark")
+  cp = SuperComputerPlayer.new
+
+  TicTacToe.new(hp, cp).run
+end
---
+++ javascript/day2/tictactoe/tic_tac_toe/lib/tic_tac_toe_node.rb
@@ -0,0 +1,61 @@
+require_relative 'tic_tac_toe'
+
+class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
+  def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+  end
+
+  def losing_node?(evaluator)
+
+    if self.board.over?
+      #won? returns false in the case of a draw.
+      return self.board.won? && self.board.winner != evaluator
+      #return false if @board.winner == evaluator
+    end
+
+    if self.next_mover_mark == evaluator #it's the player's turn
+      # no matter where we go they can force a loss 
+      self.children.all? { |child| child.losing_node?(evaluator) }
+    else #other player's turn - they can go somewhere that will force a loss
+      self.children.any? { |child| child.losing_node?(evaluator) }
+    end
+
+  end
+
+  def winning_node?(evaluator)
+    if self.board.over?
+      return self.board.winner == evaluator
+    end
+
+    if self.next_mover_mark == evaluator #it's the player's turn
+      # are any moves winning?
+      self.children.any? { |child| child.winning_node?(evaluator) }
+    else #other player's turn - no matter where they go we can force a win
+      self.children.all? { |child| child.winning_node?(evaluator) }
+    end
+
+  end
+
+  # This method generates an array of all moves that can be made after
+  # the current move.
+  def children
+    game_state_nodes = []
+
+    0.upto(2) do |i|
+      0.upto(2) do |j|
+        if @board.empty?([i, j])
+          new_board = @board.dup
+          new_board[[i, j]] = @next_mover_mark
+          new_mover_mark = @next_mover_mark == :x ? :o : :x
+          game_state_nodes << TicTacToeNode.new(new_board, new_mover_mark, [i, j])
+        end
+      end
+    end
+
+    game_state_nodes
+  end
+end
