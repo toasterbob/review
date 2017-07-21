5795ed25eaeef05c91b39600bc924e2f8c93ba91
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-21 16:08:40 -0700

    crawler

--- aA/crawler/crawl.rb
+++ aA/crawler/crawl.rb
@@ -1,28 +1,33 @@
 require 'open-uri'
 require 'json'
+require 'Nokogiri'
 
 def crawl
+  doc = Nokogiri::HTML(open('https://scottduane.github.io/TopSecretClue/'))
 
-  page = open('https://scottduane.github.io/TopSecretClue')
+  links = doc.css('li a').map { |link| link['href'] }
+  # page = open('https://scottduane.github.io/TopSecretClue')
 
-  page.each do |line|
-    if line.include?("https")
-      
+  links.each do |line|
+
+    begin
+      r = open(line)
+      if r.status[0] == "200"
+        p line
+      end
+    rescue
+      next
     end
 
-    if r.status[0] == "200"
-      doc = ""
 
-      r.each do |line|
-        p line
-        doc << line
-      end
+    # r = open("http://scottduane.github.io/SecretClue4999")
+    # r = open(line)
 
       # doc = JSON.parse(doc, :symbolize_names => true)
       # response = doc[:value][:joke]
       #
       # return response
-    end
+    # end
 
   end
 end
