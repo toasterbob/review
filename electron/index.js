aa3c1e324322e20d8918d96a2eeb155047843ce0
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-22 13:06:38 -0700

    electron renderer.js and css

--- electron/index.js
+++ electron/index.js
@@ -4,7 +4,15 @@ app.on('ready', () => {
   const mainWindow = new BrowserWindow({})
 
   const file = 'file://' + __dirname + '/index.html'
+
   mainWindow.loadURL(file)
+
+  mainWindow.webContents.on('will-navigate', (e, url) => {
+    e.preventDefault()
+    console.log(url)
+    
+    mainWindow.webContents.send('open-file', url.slice(7))
+  })
 })
 
 // console.log('hi')
--- electron/renderer.js
+++ electron/renderer.js
@@ -1,4 +1,6 @@
 const loader = require('monaco-loader')
+const { ipcRenderer } = require('electron')
+const fs = require('fs')
 
 loader().then((monaco) => {
   const div = document.getElementById('container')
@@ -7,5 +9,11 @@ loader().then((monaco) => {
     language: 'javascript',
     theme: 'vs-dark',
     automaticLayout: true
+  })
+
+  ipcRenderer.on('open-file', (e, url) => {
+    const data = fs.readFileSync(url, 'utf-8')
+
+    editor.setModel(monaco.editor.createModel(data, 'javascript'))
   })
 })
