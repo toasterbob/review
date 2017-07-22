b01af38f0a0b2cab1c6f2617b65b4101e8590398
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-22 11:37:23 -0700

    electron renderer.js and css

--- electron/renderer.js
+++ electron/renderer.js
@@ -3,5 +3,9 @@ const loader = require('monaco-loader')
 loader().then((monaco) => {
   const div = document.getElementById('container')
 
-  let editor = monaco.editor.create(div, )
+  let editor = monaco.editor.create(div, {
+    language: 'javascript',
+    theme: 'vs-dark',
+    automaticLayout: true
+  })
 })
