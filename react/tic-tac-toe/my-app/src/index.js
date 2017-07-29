fc8542baab31433b5ad0564579080dad028f6775
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-28 22:40:24 -0700

    ttt click handler and state

--- react/tic-tac-toe/my-app/src/index.js
+++ react/tic-tac-toe/my-app/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      value: null
+    }
+  }
+
   render() {
     return (
       <button className="square" onClick={() => alert('click ' + this.props.value)}>
