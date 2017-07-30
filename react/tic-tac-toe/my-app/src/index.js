878bcaf09cd5e7b65e46a83ab07e7eaed0bea51a
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-30 11:32:33 +0530

    ttt add board array to state

--- react/tic-tac-toe/my-app/src/index.js
+++ react/tic-tac-toe/my-app/src/index.js
@@ -3,16 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      value: null
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.state.value}
       </button>
     );
