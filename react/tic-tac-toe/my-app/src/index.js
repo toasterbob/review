c3d9df554423816959c1a39e06bc0a3829f0701d
Mark Noizumi <mark.noizumi@gmail.com>
2017-08-05 23:25:40 -0700

    ttt restart

--- react/tic-tac-toe/my-app/src/index.js
+++ react/tic-tac-toe/my-app/src/index.js
@@ -3,35 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  // TODO: remove the constructor
-  constructor() {
-    super();
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
-    // TODO: use onClick={this.props.onClick}
-    // TODO: replace this.state.value with this.props.value
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square">
+        {/* TODO */}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      squares: Array(9).fill(null),
-    };
-  }
-
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square />;
   }
 
   render() {
@@ -41,13 +24,19 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
@@ -76,4 +65,3 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
- 
