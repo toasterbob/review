1ebc5b90a5d922b4fc2b089b0c8004011c0e5c5a
Mark Noizumi <mark.noizumi@gmail.com>
2017-08-06 22:33:37 -0700

    ttt check for winner or an existing move in handleClick

--- react/tic-tac-toe/my-app/src/index.js
+++ react/tic-tac-toe/my-app/src/index.js
@@ -22,7 +22,11 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : "O";
+
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
