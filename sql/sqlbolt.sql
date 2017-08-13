ba6f93b6c27b610452a223fd61aa9e801f7aaa4a
Mark Noizumi <mark.noizumi@gmail.com>
2017-08-13 12:56:55 +0530

    SQL Bolt lesson 3

--- sql/sqlbolt.sql
+++ sql/sqlbolt.sql
@@ -10,3 +10,13 @@ SELECT * FROM movies WHERE id = 6;
 SELECT * FROM movies WHERE year BETWEEN 2000 AND 2010;
 SELECT * FROM movies WHERE year NOT BETWEEN 2000 AND 2010;
 SELECT * FROM movies WHERE id BETWEEN 1 AND 5;
+-- or
+SELECT title, year FROM movies WHERE year <= 2003;
+
+-- Lesson 3
+SELECT * FROM movies WHERE title LIKE "%TOY STORY%";
+SELECT * FROM movies WHERE director = "John Lasseter";
+SELECT * FROM movies WHERE director != "John Lasseter";
+SELECT * FROM movies WHERE title LIKE "WALL-_";
+
+-- Lesson 4
