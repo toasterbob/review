44add5c0b866143add361887059302c09b00c34b
Mark Noizumi <mark.noizumi@gmail.com>
2017-08-13 12:49:02 +0530

    SQL Bolt lesson 2

--- sql/sqlbolt.sql
+++ sql/sqlbolt.sql
@@ -6,3 +6,7 @@ SELECT title, year FROM movies;
 SELECT * FROM movies;
 
 -- Lesson 2
+SELECT * FROM movies WHERE id = 6;
+SELECT * FROM movies WHERE year BETWEEN 2000 AND 2010;
+SELECT * FROM movies WHERE year NOT BETWEEN 2000 AND 2010;
+SELECT * FROM movies WHERE id BETWEEN 1 AND 5;
