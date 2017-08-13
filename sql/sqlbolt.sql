cd9f7e9ab60fe59c490dc0c7c5b058d0b2602144
Mark Noizumi <mark.noizumi@gmail.com>
2017-08-13 13:30:25 +0530

    SQL Bolt lesson 6

--- sql/sqlbolt.sql
+++ sql/sqlbolt.sql
@@ -33,3 +33,8 @@ SELECT * FROM north_american_cities WHERE latitude < 21 ORDER BY population DESC
 SELECT * FROM north_american_cities WHERE latitude BETWEEN 22 AND 42 ORDER BY population DESC LIMIT 2 OFFSET 2
 
 -- Lesson 6
+SELECT title, domestic_sales, international_sales FROM movies INNER JOIN boxoffice on movies.id=boxoffice.movie_id;
+SELECT title, domestic_sales, international_sales FROM movies INNER JOIN boxoffice on movies.id=boxoffice.movie_id WHERE international_sales > domestic_sales;
+SELECT title, rating FROM movies INNER JOIN boxoffice on movies.id=boxoffice.movie_id ORDER BY rating DESC
+
+-- Lesson 7
