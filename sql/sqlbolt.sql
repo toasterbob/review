882171095128b1d077068a2ad45d45941ea285bb
Mark Noizumi <mark.noizumi@gmail.com>
2017-08-18 23:02:09 +0530

    SQL Bolt

--- sql/sqlbolt.sql
+++ sql/sqlbolt.sql
@@ -38,4 +38,3 @@ SELECT title, domestic_sales, international_sales FROM movies INNER JOIN boxoffi
 SELECT title, rating FROM movies INNER JOIN boxoffice on movies.id=boxoffice.movie_id ORDER BY rating DESC
 
 -- Lesson 7
- 
