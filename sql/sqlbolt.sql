208c92d2e8e859284d28af6dcf4c8868e221cbc5
Mark Noizumi <mark.noizumi@gmail.com>
2017-08-21 20:55:36 -0700

    SQL Bolt Lesson 13

--- sql/sqlbolt.sql
+++ sql/sqlbolt.sql
@@ -77,3 +77,19 @@ FROM mytable
 
 SELECT DISTINCT director, COUNT(*) FROM movies GROUP BY director;
 SELECT DISTINCT director, SUM(domestic_sales + international_sales) FROM movies JOIN boxoffice on id=movie_id GROUP BY director;
+
+-- Lesson 13
+INSERT INTO movies
+(title, director, year)
+VALUES
+("Toy Story 4", "John Lasseter", 2018)
+
+INSERT INTO movies VALUES (4, "Toy Story 4", "El Directore", 2015, 90);
+
+INSERT INTO boxoffice
+(movie_id, rating, domestic_sales, international_sales)
+VALUES
+(19, 8.7, 340000000, 270000000)
+INSERT INTO boxoffice VALUES (4, 8.7, 340000000, 270000000);
+
+-- Lesson 14
