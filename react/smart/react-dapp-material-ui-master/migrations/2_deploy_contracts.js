50fbcafc09771931f925f0d37c6df9b90696e627
Mark Noizumi <mark.noizumi@gmail.com>
2017-08-24 19:41:12 -0700

    smart contract

--- react/smart/react-dapp-material-ui-master/migrations/2_deploy_contracts.js
+++ react/smart/react-dapp-material-ui-master/migrations/2_deploy_contracts.js
@@ -1,11 +1,16 @@
-var Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
-var Killable = artifacts.require("./zeppelin/lifecycle/Killable.sol");
-var Authentication = artifacts.require("./Authentication.sol");
+// var Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
+// var Killable = artifacts.require("./zeppelin/lifecycle/Killable.sol");
+// var Authentication = artifacts.require("./Authentication.sol");
+//
+// module.exports = function(deployer) {
+//   deployer.deploy(Ownable);
+//   deployer.link(Ownable, Killable);
+//   deployer.deploy(Killable);
+//   deployer.link(Killable, Authentication);
+//   deployer.deploy(Authentication);
+// };
+var Adoption = artifacts.require("./Adoption.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Ownable);
-  deployer.link(Ownable, Killable);
-  deployer.deploy(Killable);
-  deployer.link(Killable, Authentication);
-  deployer.deploy(Authentication);
+  deployer.deploy(Adoption);
 };
