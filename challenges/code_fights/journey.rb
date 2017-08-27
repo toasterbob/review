5561d35f46331f5a2bb6f1b7f31b64c953af4f8e
Mark Noizumi <mark.noizumi@gmail.com>
2017-08-26 19:16:48 -0700

    code fights: century from year

--- challenges/code_fights/journey.rb
+++ challenges/code_fights/journey.rb
@@ -1,3 +1,13 @@
 def add(param1, param2)
     param1 + param2
 end
+
+#century from year
+def centuryFromYear(year)
+    return year / 100 if year % 100 == 0
+    year / 100 + 1
+end
+
+def centuryFromYear(year)
+    year % 100 == 0 ? year/100 : year/100 + 1
+end
