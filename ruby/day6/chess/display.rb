725b8410dc34f0f5361c55756a8abd8669b2a00f
Mark Noizumi <mark.noizumi@gmail.com>
2017-08-27 13:09:58 +0530

    chess Gemfile

--- ruby/day6/chess/display.rb
+++ ruby/day6/chess/display.rb
@@ -1,5 +1,6 @@
 require_relative 'board'
 require_relative 'cursor'
+require 'colorize'
 
 class Display
 
@@ -7,9 +8,14 @@ class Display
     @cursor = Cursor.new([0,0], board)
   end
 
+  def render
+    "Hello".colorize(:color => :black, :background => :white)
+
+  end
+
 end
 
 if __FILE__ == $PROGRAM_NAME
   d = Display.new
-  
+  d.render
 end
