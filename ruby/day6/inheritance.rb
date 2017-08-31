cac14e1f556e037fc9f5a7f7c7ae621acbfdaad0
Mark Noizumi <mark.noizumi@gmail.com>
2017-08-30 17:07:31 -0700

    Inheritance

---
+++ ruby/day6/inheritance.rb
@@ -0,0 +1,41 @@
+class User
+  attr_reader :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name, @last_name = first_name, last_name
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def upvote_article(article)
+    article.upvotes += 1
+  end
+end
+
+class SuperUser < User
+  attr_reader :super_powers
+
+  def initialize(first_name, last_name, super_powers)
+    super(first_name, last_name)
+    @super_powers = super_powers
+  end
+
+  def upvote_article(article)
+    # extra votes
+    article.upvotes += 3
+  end
+
+  def delete_user(user)
+    return unless super_powers.include?(:user_deletion)
+
+    # super user is authorized to delete user
+    puts "Goodbye, #{user.full_name}!"
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  u = User.new("Jamis", "Buck")
+  p u.full_name
+end
