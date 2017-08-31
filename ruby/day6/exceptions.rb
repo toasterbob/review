6dcde6d03f5f76b773cb3c64b813f309a5189d59
Mark Noizumi <mark.noizumi@gmail.com>
2017-08-31 15:27:11 -0700

    Exceptions and error handling

--- ruby/day6/exceptions.rb
+++ ruby/day6/exceptions.rb
@@ -68,6 +68,49 @@ def echo_name
   end
 end
 
+# Method and class definitions are implicitly wrapped in a begin/end block,
+# so if your error handling applies to the whole method, all you have to add is rescue
+def slope(pos1, pos2)
+  (pos2.y - pos1.y) / (pos2.x - pos1.x)
+rescue ZeroDivisionError
+  nil
+end
+
+def echo_name
+  fname, lname = prompt_name
+
+  puts "Hello #{fname} of #{lname}"
+rescue
+  puts "Please only use two names."
+  retry
+end
+
+
+# If you want your user to be able to distinguish different failure types
+# (perhaps to handle them differently), you can extend StandardError and
+# write your own
+
+class EngineStalledError < StandardError
+end
+
+class CollisionOccurredError < StandardError
+end
+
+def drive_car
+  # engine may stall, collision may occur
+end
+
+begin
+  drive_car
+rescue EngineStalledError => e
+  puts "Rescued from engine stalled!"
+rescue CollisionOccurredError => e
+  puts "Rescued from collision!"
+ensure
+  puts "Car stopped."
+end
+
+
 if __FILE__ == $PROGRAM_NAME
   # main
   echo_name
