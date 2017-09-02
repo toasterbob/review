0c748f1a7e92bf777e5a6aa5ba6d6eca5be0d4bd
Mark Noizumi <mark.noizumi@gmail.com>
2017-09-02 14:14:36 -0700

    employees and manager class

--- ruby/day6/class_inheritance/employee.rb
+++ ruby/day6/class_inheritance/employee.rb
@@ -38,8 +38,12 @@ class Manager < Employee
 end
 
 if __FILE__ == $PROGRAM_NAME
-  ned = Manager.new("Ned", "Founder", 1_000_000, nil)
-  darren = Manager.new("Darren", "TA Manager", 78_000, ned)
-  shawna = Employee.new("Shawna", "TA", 12_000, darren)
-  david = Employee.new("David", "TA", 12_000, darren)
+  p ned = Manager.new("Ned", "Founder", 1_000_000, nil)
+  p darren = Manager.new("Darren", "TA Manager", 78_000, ned)
+  p shawna = Employee.new("Shawna", "TA", 12_000, darren)
+  p david = Employee.new("David", "TA", 10_000, darren)
+
+  p ned.bonus(5) # => 500_000
+  # p darren.bonus(4) # => 88_000
+  p david.bonus(3) # => 30_000
 end
