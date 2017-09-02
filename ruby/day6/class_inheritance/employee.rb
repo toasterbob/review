7149b27be80cdb029a941d7dcc0230c95d9324a7
Mark Noizumi <mark.noizumi@gmail.com>
2017-09-02 13:56:15 -0700

    employees class

--- ruby/day6/class_inheritance/employee.rb
+++ ruby/day6/class_inheritance/employee.rb
@@ -1,12 +1,42 @@
 
 class Employee
-  
+  attr_accessor :salary
+
 
   def initialize(name, title, salary, boss)
     @name = name
     @title = title
     @salary = salary
     @boss = boss
+    boss.employees << self
+  end
+
+  def bonus(multiplier)
+    bonus = (salary) * multiplier
   end
 
 end
+
+class Manager < Employee
+  attr_accessor :salary, employees
+
+  def initialize(name, title, salary, boss, employees)
+    super(name, title, salary, boss)
+    @employees = employees
+  end
+
+  def bonus(multiplier)
+    total = 0
+    employees.each do |emp|
+      if emp.employees
+        total += emp.bonus(multiplier)
+      end
+      total += emp.salary
+    end
+    total
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+
+end 
