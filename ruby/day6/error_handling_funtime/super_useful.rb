90cb2dbfa21f07d5c0d5a7d50f53c5d0f9c9b3e0
Mark Noizumi <mark.noizumi@gmail.com>
2017-09-01 17:54:36 -0700

    convert_to_int

--- ruby/day6/error_handling_funtime/super_useful.rb
+++ ruby/day6/error_handling_funtime/super_useful.rb
@@ -1,7 +1,26 @@
 # PHASE 2
 def convert_to_int(str)
-  Integer(str)
+  begin
+    Integer(str)
+  rescue ArgumentError => e
+    puts e
+    return nil
+  end
 end
+
+# theirs 
+# def convert_to_int(str)
+#   begin
+#     num = Integer(str)
+#   rescue ArgumentError
+#     puts "Cannot convert to Integer. Please ensure you pass a valid numeric string"
+#   ensure
+#     num ||= nil
+#   end
+#
+#   num
+# end
+
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
@@ -9,9 +28,9 @@ FRUITS = ["apple", "banana", "orange"]
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
-  end 
+  else
+    raise StandardError
+  end
 end
 
 def feed_me_a_fruit
@@ -19,8 +38,8 @@ def feed_me_a_fruit
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
-end  
+  reaction(maybe_fruit)
+end
 
 # PHASE 4
 class BestFriend
@@ -39,8 +58,6 @@ class BestFriend
   end
 
   def give_friendship_bracelet
-    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me."
   end
 end
-
-
