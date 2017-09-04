51abafcaaaec13f73d0424f15835e902d7fc814f
Mark Noizumi <mark.noizumi@gmail.com>
2017-09-04 00:48:43 -0700

    chess board #move_piece and #valid_move?

--- ruby/day6/chess/game.rb
+++ ruby/day6/chess/game.rb
@@ -10,8 +10,14 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   b = Board.new
+
+  #phase1 test
   p b[[0,0]]
   start, end_pos = [0,0], [4,4]
   p b.move_piece(start, end_pos)
   p b
+  p b.move_piece(start, end_pos)
+
+  #phase2 test
+
 end
