797019d9248d3d4de95fc1167ed8c1a7396baf7c
Mark Noizumi <mark.noizumi@gmail.com>
2017-09-03 22:45:27 -0700

    not so private

--- ruby/day6/not_so_private.rb
+++ ruby/day6/not_so_private.rb
@@ -9,3 +9,9 @@ if __FILE__ == $PROGRAM_NAME
   foo = A.new
   foo.send :not_so_private_method #Hello World
 end
+
+
+# In Ruby, these distinctions are just advice from one programmer to another. 
+# Non-public methods are a way of saying "I reserve the right to change this;
+# don't depend on it." But you still get the sharp scissors of send and can
+# call any method you like.
