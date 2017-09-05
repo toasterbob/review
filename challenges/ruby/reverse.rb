090db931288231d7a37a608827b77664c080a0f3
Mark Noizumi <mark.noizumi@gmail.com>
2017-09-05 15:49:18 -0700

    reverse inplace

---
+++ challenges/ruby/reverse.rb
@@ -0,0 +1,9 @@
+class String
+  def reverse_inplace!
+    half_length = self.length / 2
+    half_length.times {|i| self[i], self[-i-1] = self[-i-1], self[i] }
+    self
+  end
+end
+
+puts "abcd".reverse_inplace!
