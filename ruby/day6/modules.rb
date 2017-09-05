cdce340db0dd6a257f3fbab4a4960f9eea280302
Mark Noizumi <mark.noizumi@gmail.com>
2017-09-04 17:53:24 -0700

    extends

--- ruby/day6/modules.rb
+++ ruby/day6/modules.rb
@@ -25,3 +25,31 @@ class Robot
     "Robot Model #2000"
   end
 end
+
+#extends
+
+module Findable
+  def objects
+    @objects ||= {}
+  end
+
+  def find(id)
+    objects[id]
+  end
+
+  def track(id, object)
+    objects[id] = object
+  end
+end
+
+class Cat
+  extend Findable
+
+  def initialize(name)
+    @name = name
+    Cat.track(@name, self)
+  end
+end
+
+Cat.new("Gizmo")
+Cat.find("Gizmo") # finds Gizmo Cat object
