d3f6147d4df4338fd640f0b9d0515fd12f5979a5
Mark Noizumi <mark.noizumi@gmail.com>
2017-09-05 23:14:25 -0700

    This is referred to as a decorator, which is used to modify a function. This Abstract Method decorator is used to define a function as Abstract.

---
+++ Object_Oriented/abstraction.py
@@ -0,0 +1,15 @@
+# We would not want to allow an object of type Animal to be created. We
+# would rather want an object of type Bird, or Dog, or Whale to be created
+# instead. Thus, we define the Animal class as Abstract, leaving Bird, Dog
+# and Whale as Concrete classes which can be instantiated as objects.
+
+from abc import ABCMeta, abstractmethod
+
+class MyAbstractClass:
+    __metaclass__ = ABCMeta #This line defines the class as a whole as Abstract.
+
+    @abstractmethod
+    # This is referred to as a decorator, which is used to modify a function. 
+    # This Abstract Method decorator is used to define a function as Abstract.
+    def getById(self, userId):
+        pass
