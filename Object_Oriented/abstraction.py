b074d106f62e0d76bc575d47220d2a979525ecb6
Mark Noizumi <mark.noizumi@gmail.com>
2017-09-05 23:21:57 -0700

    Abstract

--- Object_Oriented/abstraction.py
+++ Object_Oriented/abstraction.py
@@ -9,7 +9,31 @@ class MyAbstractClass:
     __metaclass__ = ABCMeta #This line defines the class as a whole as Abstract.
 
     @abstractmethod
-    # This is referred to as a decorator, which is used to modify a function. 
+    # This is referred to as a decorator, which is used to modify a function.
     # This Abstract Method decorator is used to define a function as Abstract.
     def getById(self, userId):
+        pass
+
+from abc import ABCMeta, abstractmethod
+
+class Animal:
+    __metaclass__ = ABCMeta
+
+    name = ""
+    location = ""
+
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
+    @abstractmethod
+    def talk():
+        pass
+
+    @abstractmethod
+    def move():
+        pass
+
+    @abstractmethod
+    def breathe():
         pass
