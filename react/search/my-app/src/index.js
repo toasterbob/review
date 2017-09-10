29ab14a2c81720e4842aed64f082e860c62571b6
Mark Noizumi <mark.noizumi@gmail.com>
2017-09-09 21:55:38 -0700

    create react project

--- react/search/my-app/src/index.js
+++ react/search/my-app/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+ReactDOM.render(
+  <h1>Hello!</h1>,
+  document.getElementById('root')
+);
