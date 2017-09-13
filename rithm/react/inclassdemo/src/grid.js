237cbf0d615fb2671965879ec056174ad2da3bfe
Mark Noizumi <mark.noizumi@gmail.com>
2017-09-12 20:23:20 -0700

    square

---
+++ rithm/react/inclassdemo/src/grid.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Square from './square';
+
+class Grid extends Component {
+  render(){
+
+    return (<div>
+            <Square />
+            </div>);
+  }
+}
+
+export default Grid
