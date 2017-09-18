f2c27256b3e29f6ceb19dc0d03ad02f0f2358270
Mark Noizumi <mark.noizumi@gmail.com>
2017-09-17 23:34:35 -0700

    setInterval with randomly change different number of squares

--- rithm/react/inclassdemo/src/square.js
+++ rithm/react/inclassdemo/src/square.js
@@ -3,13 +3,13 @@ import React, {Component} from 'react';
 class Square extends Component {
   render(){
     const style = {
-      width: '50px',
-      height: '50px',
+      width: '40px',
+      height: '40px',
       display: 'inline-block',
       backgroundColor: this.props.color
     };
     return (<div style={style}>
-              hi
+              <br/>
             </div>);
   }
 }
